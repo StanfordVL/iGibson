cb0283429d79d21a01f9b0f4ef4a91cb5330f6e5
fxia22 <xf1280@gmail.com>
2019-01-06 15:39:37 -0800

    refactor, add multiagent

--- test/test_robot.py
+++ test/test_robot.py
@@ -1,12 +1,14 @@
 import yaml
 from gibson2.core.physics.robot_locomotors import *
-from gibson2.core.physics.simulator import Simulator
+from gibson2.core.simulator import Simulator
 from gibson2.core.physics.scene import *
+
 
 def parse_config(config):
     with open(config, 'r') as f:
         config_data = yaml.load(f)
     return config_data
+
 
 config = parse_config('test.yaml')
 
@@ -19,6 +21,7 @@ def test_turtlebot():
     assert p.getNumBodies() == 5
     s.disconnect()
 
+
 def test_jr2():
     s = Simulator()
     scene = StadiumScene()
@@ -26,6 +29,7 @@ def test_jr2():
     jr2 = JR2(config)
     assert p.getNumBodies() == 5
     s.disconnect()
+
 
 def test_ant():
     s = Simulator()
@@ -35,6 +39,7 @@ def test_ant():
     assert p.getNumBodies() == 5
     s.disconnect()
 
+
 def test_husky():
     s = Simulator()
     scene = StadiumScene()
@@ -42,6 +47,7 @@ def test_husky():
     husky = Husky(config)
     assert p.getNumBodies() == 5
     s.disconnect()
+
 
 def test_humanoid():
     s = Simulator()
@@ -51,6 +57,7 @@ def test_humanoid():
     assert p.getNumBodies() == 5
     s.disconnect()
 
+
 def test_quadrotor():
     s = Simulator()
     scene = StadiumScene()
@@ -58,6 +65,7 @@ def test_quadrotor():
     quadrotor = Quadrotor(config)
     assert p.getNumBodies() == 5
     s.disconnect()
+
 
 def test_turtlebot_position():
     s = Simulator()
@@ -67,8 +75,9 @@ def test_turtlebot_position():
     assert p.getNumBodies() == 5
 
     turtlebot.set_position([0, 0, 5])
-    assert np.allclose(turtlebot.get_position(),  np.array([0,0,5]))
+    assert np.allclose(turtlebot.get_position(), np.array([0, 0, 5]))
     s.disconnect()
+
 
 def test_humanoid_position():
     s = Simulator()
@@ -77,5 +86,26 @@ def test_humanoid_position():
     humanoid = Humanoid(config)
     assert p.getNumBodies() == 5
     humanoid.set_position([0, 0, 5])
-    assert np.allclose(humanoid.get_position(),  np.array([0,0,5]))
+    assert np.allclose(humanoid.get_position(), np.array([0, 0, 5]))
     s.disconnect()
+
+
+def test_multiagent():
+    s = Simulator()
+    scene = StadiumScene()
+    s.import_scene(scene)
+    turtlebot1 = Turtlebot(config)
+    turtlebot2 = Turtlebot(config)
+    turtlebot3 = Turtlebot(config)
+
+    turtlebot1.set_position([1, 0, 0.5])
+    turtlebot2.set_position([0, 0, 0.5])
+    turtlebot3.set_position([-1, 0, 0.5])
+
+    assert p.getNumBodies() == 7
+
+    for i in range(1000):
+        turtlebot1.apply_action(1)
+        turtlebot2.apply_action(1)
+        turtlebot3.apply_action(1)
+        s.step()
