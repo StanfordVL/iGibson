ce86122648d5007e1face3194eccae6aa55ac076
fxia22 <xf1280@gmail.com>
2019-01-06 14:34:18 -0800

    add rendering unit test

---
+++ test/test_binding.py
@@ -0,0 +1,13 @@
+from gibson2.core.render.mesh_renderer import CppMeshRenderer
+from gibson2.core.render.mesh_renderer.get_available_devices import get_available_devices
+
+
+def test_device():
+    assert len(get_available_devices()) > 0
+
+
+def test_binding():
+    r = CppMeshRenderer.CppMeshRenderer(256, 256, get_available_devices()[0])
+    r.init()
+    r.query()
+    r.release()
