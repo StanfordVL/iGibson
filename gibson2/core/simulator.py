ce8e13904b34a650b42329279818971965a10827
fxia22 <xf1280@gmail.com>
2019-01-08 14:04:37 -0800

    add viewer

--- gibson2/core/simulator.py
+++ gibson2/core/simulator.py
@@ -2,6 +2,7 @@ import pybullet as p
 from gibson2.core.physics.scene import StadiumScene, BuildingScene
 from gibson2.core.render.mesh_renderer.mesh_renderer_cpu import *
 from gibson2.core.physics.interactive_objects import *
+from gibson2.core.viewer import Viewer
 
 class Simulator:
     def __init__(self, gravity=9.8, timestep=1 / 240.0):
@@ -18,6 +19,12 @@ class Simulator:
         self.renderer = MeshRenderer(width=300, height=300)
         self.renderer.set_fov(90)
         self.visual_objects = {}
+
+        self.add_viewer()
+
+    def add_viewer(self):
+        self.viewer = Viewer()
+        self.viewer.renderer = self.renderer
 
     def import_scene(self, scene):
         new_objects = scene.load()
@@ -52,8 +59,21 @@ class Simulator:
 
     def import_robot(self, robot):
         ids = robot.load()
+        visual_objects = []
+        link_ids = []
         for shape in p.getVisualShapeData(ids[0]):
-            print(shape)
+            id, link_id, type, _, filename = shape[:5]
+            if type == p.GEOM_MESH:
+                filename = filename.decode('utf-8')
+                if not filename in self.visual_objects.keys():
+                    print(filename)
+                    self.renderer.load_object(filename)
+                    visual_objects.append(len(self.renderer.visual_objects) - 1)
+                    self.visual_objects[filename] = len(self.renderer.visual_objects) - 1
+                else:
+                    visual_objects.append(self.visual_objects[filename])
+                link_ids.append(link_id)
+        self.renderer.add_instance_group(visual_objects, link_ids, ids[0], dynamic=True)
         return ids
 
     def step(self):
@@ -61,13 +81,23 @@ class Simulator:
         for instance in self.renderer.instances:
             if instance.dynamic:
                 self.update_position(instance)
+        self.viewer.update()
+
 
     @staticmethod
     def update_position(instance):
-        pos, orn = p.getBasePositionAndOrientation(instance.pybullet_uuid)
-        instance.set_position(pos)
-        instance.set_rotation([orn[-1], orn[0], orn[1], orn[2]])
+        if isinstance(instance, Instance):
+            pos, orn = p.getBasePositionAndOrientation(instance.pybullet_uuid)
+            instance.set_position(pos)
+            instance.set_rotation([orn[-1], orn[0], orn[1], orn[2]])
+        elif isinstance(instance, InstanceGroup):
+            for link_id in instance.link_ids:
+                if link_id == -1:
+                    pos, orn = p.getBasePositionAndOrientation(instance.pybullet_uuid)
+                else:
+                    _, _, _, _, pos, orn = p.getLinkState(instance.pybullet_uuid, link_id)
 
+                print(instance.pybullet_uuid, link_id, pos, orn)
     def isconnected(self):
         return p.getConnectionInfo(self.cid)['isConnected']
 
