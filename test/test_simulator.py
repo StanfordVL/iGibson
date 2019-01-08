512c89fea51f152fc9d6a274b8bf923ae05218c1
fxia22 <xf1280@gmail.com>
2019-01-07 20:48:33 -0800

    add one more test

---
+++ test/test_simulator.py
@@ -0,0 +1,21 @@
+import pybullet as p
+from gibson2.core.physics.scene import StadiumScene, BuildingScene
+from gibson2.core.render.mesh_renderer.mesh_renderer_cpu import *
+from gibson2.core.physics.interactive_objects import *
+from gibson2.core.simulator import Simulator
+
+def test_simulator():
+    s = Simulator()
+    scene = StadiumScene()
+    s.import_scene(scene)
+    obj = YCBObject('006_mustard_bottle')
+
+    for i in range(10):
+        s.import_object(obj)
+
+    obj = YCBObject('002_master_chef_can')
+    for i in range(10):
+        s.import_object(obj)
+
+    for i in range(1000):
+        s.step()
