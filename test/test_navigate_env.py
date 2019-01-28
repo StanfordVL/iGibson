c18e6cb74588e10215fcf4683ee47e4d48229b1c
fxia22 <xf1280@gmail.com>
2019-01-28 15:04:21 -0800

    add tf_agents wrapper

--- test/test_navigate_env.py
+++ test/test_navigate_env.py
@@ -6,10 +6,14 @@ from gibson2.utils.utils import parse_config
 from gibson2.envs.base_env import BaseEnv
 from gibson2.envs.locomotor_env import *
 from time import time
+
+from tf_agents.environments import gym_wrapper
+
 def test_env():
     config_filename = os.path.join(os.path.dirname(gibson2.__file__), '../test/test.yaml')
     nav_env = NavigateEnv(config_file=config_filename, mode='headless')
-    for j in range(5):
+
+    for j in range(2):
         nav_env.reset()
         for i in range(300): # 300 steps, 30s world time
             s = time()
@@ -19,3 +23,15 @@ def test_env():
             if ts[2]:
                 print("Episode finished after {} timesteps".format(i + 1))
                 break
+
+def test_wrapper():
+    config_filename = os.path.join(os.path.dirname(gibson2.__file__), '../test/test.yaml')
+    nav_env = NavigateEnv(config_file=config_filename, mode='headless')
+    tfenv = gym_wrapper.GymWrapper(
+        nav_env,
+        discount=1,
+        spec_dtype_map=None,
+        auto_reset=True,
+    )
+    print("action spec", tfenv.action_spec())
+    print("observation spec", tfenv.observation_spec())
