4cb1e4ae1841872be952b0fa45a782e456eeece6
Kevin Chen <kevin.chen@cs.stanford.edu>
2019-04-29 17:12:06 -0700

    support adding shapenet objects

--- gibson2/core/physics/interactive_objects.py
+++ gibson2/core/physics/interactive_objects.py
@@ -1,6 +1,38 @@
 import pybullet as p
 from gibson2 import assets
+import numpy as np
 import os
+
+
+class ShapeNetObject(object):
+    def __init__(self, path, scale=1., position=[0, 0, 0], orientation=[0, 0, 0]):
+        self.filename =  path
+        self.scale = scale
+        self.position = position
+        self.orientation = orientation
+
+        self._default_mass = 3.
+        self._default_transform = {
+            'position': [0, 0, 0],
+            'orientation_quat': [1. / np.sqrt(2), 0, 0, 1. / np.sqrt(2)],
+            }
+        pose = p.multiplyTransforms(positionA=self.position,
+                                    orientationA=p.getQuaternionFromEuler(self.orientation),
+                                    positionB=self._default_transform['position'],
+                                    orientationB=self._default_transform['orientation_quat'])
+        self.pose = {
+            'position': pose[0],
+            'orientation_quat': pose[1],
+            }
+
+    def load(self):
+        collision_id = p.createCollisionShape(p.GEOM_MESH, fileName=self.filename, meshScale=self.scale)
+        body_id = p.createMultiBody(basePosition=self.pose['position'],
+                                    baseOrientation=self.pose['orientation_quat'],
+                                    baseMass=self._default_mass,
+                                    baseCollisionShapeIndex=collision_id,
+                                    baseVisualShapeIndex=-1)
+        return body_id
 
 class YCBObject:
     def __init__(self, name, scale=1):
