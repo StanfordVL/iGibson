bb1b0c7b5f65ca6171b34e826e7a8f311dbfa26e
Chengshu Li <chengshu@capri11.stanford.edu>
2019-06-21 13:50:31 -0700

    add subgoal visualization, observation normalization, redice action dim

--- gibson2/envs/parallel_env.py
+++ gibson2/envs/parallel_env.py
@@ -96,6 +96,12 @@ class ParallelNavEnvironment(NavigateEnv):
         for env in self._envs:
             env.close()
 
+    def set_subgoal(self, subgoals):
+        time_steps = [env.set_subgoal(subgoal, self._blocking) for env, subgoal in zip(self._envs, subgoals)]
+        if not self._blocking:
+            time_steps = [promise() for promise in time_steps]
+        return time_steps
+
     #def _stack_time_steps(self, time_steps):
     #  """Given a list of TimeStep, combine to one with a batch dimension."""
     #  if self._flatten:
@@ -265,6 +271,13 @@ class ProcessPyEnvironment(object):
         else:
             return promise
 
+    def set_subgoal(self, subgoal, blocking=True):
+        promise = self.call('set_subgoal', subgoal)
+        if blocking:
+            return promise()
+        else:
+            return promise
+
     def _receive(self):
         """Wait for a message from the worker process and return its payload.
 
@@ -319,7 +332,7 @@ class ProcessPyEnvironment(object):
                     continue
                 if message == self._CALL:
                     name, args, kwargs = payload
-                    if name == 'step' or name == 'reset':
+                    if name == 'step' or name == 'reset' or name == 'set_subgoal':
                         result = getattr(env, name)(*args, **kwargs)
                     #result = []
                     #if flatten and name == 'step' or name == 'reset':
