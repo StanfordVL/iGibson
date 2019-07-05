31d79a8bba5faa5b4d0357d2490ed7a7a2e2d2a7
fxia22 <xf1280@gmail.com>
2019-07-04 22:40:55 -0700

    change load order

--- gibson2/core/simulator.py
+++ gibson2/core/simulator.py
@@ -45,6 +45,11 @@ class Simulator:
         self.load()
 
     def load(self):
+        self.renderer = MeshRenderer(width=self.resolution,
+                                     height=self.resolution,
+                                     device_idx=self.device_idx,
+                                     use_fisheye=self.use_fisheye)
+        
         if self.mode == 'gui':
             self.cid = p.connect(p.GUI)
         else:
@@ -52,10 +57,7 @@ class Simulator:
         p.setTimeStep(self.timestep)
         p.setGravity(0, 0, -self.gravity)
 
-        self.renderer = MeshRenderer(width=self.resolution,
-                                     height=self.resolution,
-                                     device_idx=self.device_idx,
-                                     use_fisheye=self.use_fisheye)
+
         self.renderer.set_fov(90)
 
         if self.mode == 'gui':
