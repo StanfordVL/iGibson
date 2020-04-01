5143e3464e652b395d7f16231f5998f6e507dec7
kenzomenzo <kent@vainionet.com>
2020-04-01 19:36:15 +0100

    Finished simple interaction stadium demo

--- examples/demo/vr_interaction_demo_inside.py
+++ examples/demo/vr_interaction_demo_inside.py
@@ -1,40 +1,45 @@
 import pybullet as p
 import yaml
 from gibson2.core.physics.robot_locomotors import Turtlebot, Fetch
-from gibson2.core.physics.scene import StadiumScene, BuildingScene
+from gibson2.core.physics.scene import StadiumScene, BuildingScene, EmptyScene
 from gibson2.core.physics.interactive_objects import YCBObject, InteractiveObj
 from gibson2.core.simulator import Simulator
 from gibson2.utils.utils import parse_config
 from gibson2 import assets_path
 import numpy as np
 
+# TODO: More work needs to be done to make Ohopee have physics!
 configs_folder = '..\\configs\\'
+ohopee_path = "..\\..\\gibson2\\assets\\datasets\\Ohoopee\\Ohoopee_mesh_texture.obj"
 bullet_obj_folder = assets_path + '\\models\\bullet_models\\data\\'
-sample_urdf_folder = assets_path + '\\models\\sample_urdfs\\'
 config = parse_config(configs_folder + 'fetch_interactive_nav.yaml')
 
 s = Simulator(mode='vr')
-scene = StadiumScene()
+scene = EmptyScene()
 s.import_scene(scene)
+
+s.renderer.load_object(ohopee_path)
+s.renderer.add_instance(0)
+
 fetch = Fetch(config)
 s.import_robot(fetch)
 fetch.set_position([0,0.5,0])
 
-# Drills represent hands
-left_drill = YCBObject('035_power_drill')
-s.import_object(left_drill)
-left_drill_id = left_drill.body_id
-print("Left drill body id: %d" % left_drill_id)
+# Poles represent hands
+# Left hand
+left_pole = InteractiveObj(bullet_obj_folder + 'pole.urdf', scale=0.7)
+s.import_object(left_pole)
+left_pole_id = left_pole.body_id
 
-right_drill = YCBObject('035_power_drill')
-s.import_object(right_drill)
-right_drill_id = right_drill.body_id
-print("Right drill body id: %d" % right_drill_id)
+lpole_cid = p.createConstraint(left_pole_id, -1, -1, -1, p.JOINT_FIXED, [0,0,0], [0,0,0], [0,0,0])
 
-def getDrillStartRotation():
-    return p.getQuaternionFromEuler([0, -1.57, -1.57])
+# Right hand
+right_pole = InteractiveObj(bullet_obj_folder + 'pole.urdf', scale=0.7)
+s.import_object(right_pole)
+right_pole_id = right_pole.body_id
 
-drill_base_orn = getDrillStartRotation()
+# TODO: Change this constraint?
+rpole_cid = p.createConstraint(right_pole_id, -1, -1, -1, p.JOINT_FIXED, [0,0,0], [0,0,0], [0,0,0])
 
 def multQuatLists(q0, q1):
     x0, y0, z0, w0 = q0
@@ -47,9 +52,6 @@ def multQuatLists(q0, q1):
 
     return [x,y,z,w]
 
-def moveHand(handId, pos, orn):
-    p.resetBasePositionAndOrientation(handId, pos, orn)
-
 obj = YCBObject('006_mustard_bottle')
 
 for i in range(2):
@@ -59,15 +61,15 @@ obj = YCBObject('002_master_chef_can')
 for i in range(2):
     s.import_object(obj)
 
+# Rotates poles to correct orientation relative to VR controller
+pole_correction_quat = p.getQuaternionFromEuler([0, 1.57, 0])
+
 # Runs simulation and measures fps
 while True:
     # Always call before step
     eventList = s.pollVREvents()
     for event in eventList:
         deviceType, eventType = event
-        if deviceType == "left_controller":
-            print(eventType)
-            print("Event happend on left controller!")
 
     # Set should_measure_fps to True to measure the current fps
     s.step(should_measure_fps=False)
@@ -78,11 +80,11 @@ while True:
     rIsValid, rTrans, rRot = s.getDataForVRDevice('right_controller')
 
     if lIsValid:
-        final_orn = multQuatLists(lRot, drill_base_orn)
-        moveHand(left_drill_id, lTrans, final_orn)
+        final_rot = multQuatLists(lRot, pole_correction_quat)
+        p.changeConstraint(lpole_cid, lTrans, final_rot, maxForce=500)
 
     if rIsValid:
-        final_orn = multQuatLists(rRot, drill_base_orn)
-        moveHand(right_drill_id, rTrans, final_orn)
+        final_rot = multQuatLists(rRot, pole_correction_quat)
+        p.changeConstraint(rpole_cid, rTrans, final_rot, maxForce=500)
         
 s.disconnect()
