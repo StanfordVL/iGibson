98c51b8bee8ef340174655e5569a45be2f35c5e5
Chengshu Li <chengshu@stanford.edu>
2020-04-04 23:34:37 -0700

    finish example and documentation for Algorithms and Environments

--- examples/demo/env_customized_example.py
+++ examples/demo/env_customized_example.py
@@ -1,4 +1,4 @@
-from gibson2.envs.locomotor_env import NavigateRandomEnv
+from gibson2.envs.locomotor_env import NavigateRandomEnvSim2Real
 from time import time
 import numpy as np
 from time import time
@@ -10,7 +10,9 @@ from gibson2.core.render.profiler import Profiler
 def main():
     config_filename = os.path.join(os.path.dirname(gibson2.__file__),
                                    '../examples/configs/turtlebot_demo.yaml')
-    nav_env = NavigateRandomEnv(config_file=config_filename, mode='gui')
+    nav_env = NavigateRandomEnvSim2Real(config_file=config_filename,
+                                        mode='gui',
+                                        track='interactive')
     for j in range(10):
         nav_env.reset()
         for i in range(100):
