ca55a6383a30fc8a91f13ca1e6b2fb377bca0994
Fei Xia <xf1280@gmail.com>
2020-05-11 20:57:08 -0700

    Update challenge.py
    `agent.reset` becomes optional as participants might forget to do it

--- gibson2/envs/challenge.py
+++ gibson2/envs/challenge.py
@@ -21,7 +21,10 @@ class Challenge:
         num_eval_episodes = 10
         for i in range(num_eval_episodes):
             print('Episode: {}/{}'.format(i + 1, num_eval_episodes))
-            agent.reset()
+            try:
+                agent.reset()
+            except:
+                pass
             state = self.nav_env.reset()
             while True:
                 action = agent.act(state)
