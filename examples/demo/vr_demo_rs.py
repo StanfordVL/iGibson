a7b43b5fbc42fe38c1ae804c7da8df14ea882fc1
kenzomenzo <kent@vainionet.com>
2020-06-05 13:13:50 +0100

    Added texture shrinking to avoid memory overload

--- examples/demo/vr_demo_rs.py
+++ examples/demo/vr_demo_rs.py
@@ -15,17 +15,20 @@ configs_folder = '..\\configs\\'
 fetch_config = parse_config(configs_folder + 'fetch_p2p_nav.yaml')
 
 optimize = True
+# Toggle this to only use renderer without VR, for testing purposes
+vrMode = True
+
 # Timestep should always be set to 1/90 to match VR system's 90fps
-s = Simulator(mode='vr', timestep = 1/90.0, msaa=True, vrFullscreen=False, optimize_render=optimize, vrMode=True)
+s = Simulator(mode='vr', timestep = 1/90.0, msaa=True, vrFullscreen=False, optimize_render=optimize, vrMode=vrMode)
 scene = BuildingScene('Rs_interactive', build_graph=False, is_interactive=True)
 scene.sleep = optimize
 s.import_scene(scene)
 
 # Fetch robot in scene
-fetch = Fetch(fetch_config)
-s.import_robot(fetch)
-fetch.set_position([0,0,0])
-fetch.robot_specific_reset()
+#fetch = Fetch(fetch_config)
+#s.import_robot(fetch)
+#fetch.set_position([0,0,0])
+#fetch.robot_specific_reset()
 
 # Grippers represent hands
 lGripper = GripperObj(gripper_folder + 'gripper.urdf')
@@ -47,33 +50,35 @@ if optimize:
 while True:
     start = time.time()
 
-    eventList = s.pollVREvents()
-    for event in eventList:
-        deviceType, eventType = event
-        if deviceType == 'left_controller':
-            if eventType == 'trigger_press':
-                leftGripperFraction = 0.8
-            elif eventType == 'trigger_unpress':
-                leftGripperFraction = 0.0
-        elif deviceType == 'right_controller':
-            if eventType == 'trigger_press':
-                rightGripperFraction = 0.8
-            elif eventType == 'trigger_unpress':
-                rightGripperFraction = 0.0
+    if vrMode:
+        eventList = s.pollVREvents()
+        for event in eventList:
+            deviceType, eventType = event
+            if deviceType == 'left_controller':
+                if eventType == 'trigger_press':
+                    leftGripperFraction = 0.8
+                elif eventType == 'trigger_unpress':
+                    leftGripperFraction = 0.0
+            elif deviceType == 'right_controller':
+                if eventType == 'trigger_press':
+                    rightGripperFraction = 0.8
+                elif eventType == 'trigger_unpress':
+                    rightGripperFraction = 0.0
 
-    s.step()
+    s.step(shouldTime=True)
 
-    hmdIsValid, hmdTrans, hmdRot = s.getDataForVRDevice('hmd')
-    lIsValid, lTrans, lRot = s.getDataForVRDevice('left_controller')
-    rIsValid, rTrans, rRot = s.getDataForVRDevice('right_controller')
+    if vrMode:
+        hmdIsValid, hmdTrans, hmdRot = s.getDataForVRDevice('hmd')
+        lIsValid, lTrans, lRot = s.getDataForVRDevice('left_controller')
+        rIsValid, rTrans, rRot = s.getDataForVRDevice('right_controller')
 
-    if lIsValid:
-        lGripper.move_gripper(lTrans, lRot)
-        lGripper.set_close_fraction(leftGripperFraction)
+        if lIsValid:
+            lGripper.move_gripper(lTrans, lRot)
+            lGripper.set_close_fraction(leftGripperFraction)
 
-    if rIsValid:
-        rGripper.move_gripper(rTrans, rRot)
-        rGripper.set_close_fraction(rightGripperFraction)
+        if rIsValid:
+            rGripper.move_gripper(rTrans, rRot)
+            rGripper.set_close_fraction(rightGripperFraction)
     
     elapsed = time.time() - start
     if (elapsed > 0):
