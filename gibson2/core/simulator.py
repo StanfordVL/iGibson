add scripts to generate GIFs to evaluate dynamics properties of objects, fix bugs: object sleeping, renderer cannot handle scaling in urdf, renderer cannot handle when base link frame is different from inertial frame

gibson2/core/simulator.py
@@ -10,7 +10,8 @@ import os
 import numpy as np
 import platform
 import logging
-        
+from IPython import embed
+
 
 class Simulator:
     def __init__(self,
@@ -46,22 +47,22 @@ class Simulator:
         self.gravity = gravity
         self.timestep = timestep
         self.mode = mode
-        
+
         plt = platform.system()
         if plt == 'Darwin' and self.mode == 'gui':
-            self.mode = 'iggui' # for mac os disable pybullet rendering
+            self.mode = 'iggui'  # for mac os disable pybullet rendering
             logging.warn('Rendering both iggui and pbgui is not supported on mac, choose either pbgui or '
-                      'iggui. Default to iggui.')
-        
+                         'iggui. Default to iggui.')
+
         self.use_pb_renderer = False
         self.use_ig_renderer = False
-        
+
         if self.mode in ['gui', 'iggui']:
             self.use_ig_renderer = True
-     
+
         if self.mode in ['gui', 'pbgui']:
             self.use_pb_renderer = True
-                   
+
         # renderer
         self.image_width = image_width
         self.image_height = image_height
@@ -97,7 +98,6 @@ class Simulator:
         self.load()
 
     def load(self):
-
         """
         Set up MeshRenderer and physics simulation client. Initialize the list of objects.
         """
@@ -146,7 +146,8 @@ class Simulator:
         return wrapped_load_func
 
     @load_without_pybullet_vis
-    def import_scene(self, scene, texture_scale=1.0, load_texture=True, class_id=None):
+    def import_scene(self, scene, texture_scale=1.0, load_texture=True,
+                     render_floor_plane=False, class_id=None):
         """
         Import a scene into the simulator. A scene could be a synthetic one or a realistic Gibson Environment.
 
@@ -170,25 +171,28 @@ class Simulator:
                 visual_object = None
                 if type == p.GEOM_MESH:
                     filename = filename.decode('utf-8')
-                    if (filename,(*dimensions)) not in self.visual_objects.keys():
+                    if (filename, (*dimensions)) not in self.visual_objects.keys():
                         self.renderer.load_object(filename,
                                                   texture_scale=texture_scale,
                                                   load_texture=load_texture)
-                        self.visual_objects[(filename,(*dimensions))] = len(self.renderer.visual_objects) - 1
-                    visual_object = self.visual_objects[(filename,(*dimensions))]
+                        self.visual_objects[(
+                            filename, (*dimensions))] = len(self.renderer.visual_objects) - 1
+                    visual_object = self.visual_objects[(
+                        filename, (*dimensions))]
                 elif type == p.GEOM_PLANE:
-                    pass
                     # By default, we add an additional floor surface to "smooth out" that of the original mesh.
                     # Normally you don't need to render this additionally added floor surface.
-                    # However, if you do want to render it for some reason, you can uncomment the block below.
-
-                    # filename = os.path.join(gibson2.assets_path, 'models/mjcf_primitives/cube.obj')
-                    # self.renderer.load_object(filename,
-                    #                           transform_orn=rel_orn,
-                    #                           transform_pos=rel_pos,
-                    #                           input_kd=color[:3],
-                    #                           scale=[100, 100, 0.01])
-                    # visual_object = len(self.renderer.visual_objects) - 1
+                    # However, if you do want to render it for some reason, you can set render_floor_plane to be True.
+                    if render_floor_plane:
+                        filename = os.path.join(
+                            gibson2.assets_path,
+                            'models/mjcf_primitives/cube.obj')
+                        self.renderer.load_object(filename,
+                                                  transform_orn=rel_orn,
+                                                  transform_pos=rel_pos,
+                                                  input_kd=color[:3],
+                                                  scale=[100, 100, 0.01])
+                        visual_object = len(self.renderer.visual_objects) - 1
 
                 if visual_object is not None:
                     self.renderer.add_instance(visual_object,
@@ -238,16 +242,18 @@ class Simulator:
             visual_object = None
             if type == p.GEOM_MESH:
                 filename = filename.decode('utf-8')
-                if (filename,(*dimensions)) not in self.visual_objects.keys():
+                if (filename, (*dimensions)) not in self.visual_objects.keys():
                     self.renderer.load_object(filename,
                                               transform_orn=rel_orn,
                                               transform_pos=rel_pos,
                                               input_kd=color[:3],
                                               scale=np.array(dimensions))
-                    self.visual_objects[(filename,(*dimensions))] = len(self.renderer.visual_objects) - 1
-                visual_object = self.visual_objects[(filename,(*dimensions))]
+                    self.visual_objects[(filename, (*dimensions))
+                                        ] = len(self.renderer.visual_objects) - 1
+                visual_object = self.visual_objects[(filename, (*dimensions))]
             elif type == p.GEOM_SPHERE:
-                filename = os.path.join(gibson2.assets_path, 'models/mjcf_primitives/sphere8.obj')
+                filename = os.path.join(
+                    gibson2.assets_path, 'models/mjcf_primitives/sphere8.obj')
                 self.renderer.load_object(
                     filename,
                     transform_orn=rel_orn,
@@ -256,7 +262,8 @@ class Simulator:
                     scale=[dimensions[0] / 0.5, dimensions[0] / 0.5, dimensions[0] / 0.5])
                 visual_object = len(self.renderer.visual_objects) - 1
             elif type == p.GEOM_CAPSULE or type == p.GEOM_CYLINDER:
-                filename = os.path.join(gibson2.assets_path, 'models/mjcf_primitives/cube.obj')
+                filename = os.path.join(
+                    gibson2.assets_path, 'models/mjcf_primitives/cube.obj')
                 self.renderer.load_object(
                     filename,
                     transform_orn=rel_orn,
@@ -265,7 +272,8 @@ class Simulator:
                     scale=[dimensions[1] / 0.5, dimensions[1] / 0.5, dimensions[0]])
                 visual_object = len(self.renderer.visual_objects) - 1
             elif type == p.GEOM_BOX:
-                filename = os.path.join(gibson2.assets_path, 'models/mjcf_primitives/cube.obj')
+                filename = os.path.join(
+                    gibson2.assets_path, 'models/mjcf_primitives/cube.obj')
                 self.renderer.load_object(filename,
                                           transform_orn=rel_orn,
                                           transform_pos=rel_pos,
@@ -306,17 +314,20 @@ class Simulator:
             id, link_id, type, dimensions, filename, rel_pos, rel_orn, color = shape[:8]
             if type == p.GEOM_MESH:
                 filename = filename.decode('utf-8')
-                if (filename,(*dimensions)) not in self.visual_objects.keys():
+                if (filename, (*dimensions)) not in self.visual_objects.keys():
                     self.renderer.load_object(filename,
                                               transform_orn=rel_orn,
                                               transform_pos=rel_pos,
                                               input_kd=color[:3],
                                               scale=np.array(dimensions))
-                    self.visual_objects[(filename,(*dimensions))] = len(self.renderer.visual_objects) - 1
-                visual_objects.append(self.visual_objects[(filename,(*dimensions))])
+                    self.visual_objects[(filename, (*dimensions))
+                                        ] = len(self.renderer.visual_objects) - 1
+                visual_objects.append(
+                    self.visual_objects[(filename, (*dimensions))])
                 link_ids.append(link_id)
             elif type == p.GEOM_SPHERE:
-                filename = os.path.join(gibson2.assets_path, 'models/mjcf_primitives/sphere8.obj')
+                filename = os.path.join(
+                    gibson2.assets_path, 'models/mjcf_primitives/sphere8.obj')
                 self.renderer.load_object(
                     filename,
                     transform_orn=rel_orn,
@@ -326,7 +337,8 @@ class Simulator:
                 visual_objects.append(len(self.renderer.visual_objects) - 1)
                 link_ids.append(link_id)
             elif type == p.GEOM_CAPSULE or type == p.GEOM_CYLINDER:
-                filename = os.path.join(gibson2.assets_path, 'models/mjcf_primitives/cube.obj')
+                filename = os.path.join(
+                    gibson2.assets_path, 'models/mjcf_primitives/cube.obj')
                 self.renderer.load_object(
                     filename,
                     transform_orn=rel_orn,
@@ -336,7 +348,8 @@ class Simulator:
                 visual_objects.append(len(self.renderer.visual_objects) - 1)
                 link_ids.append(link_id)
             elif type == p.GEOM_BOX:
-                filename = os.path.join(gibson2.assets_path, 'models/mjcf_primitives/cube.obj')
+                filename = os.path.join(
+                    gibson2.assets_path, 'models/mjcf_primitives/cube.obj')
                 self.renderer.load_object(filename,
                                           transform_orn=rel_orn,
                                           transform_pos=rel_pos,
@@ -392,17 +405,20 @@ class Simulator:
             id, link_id, type, dimensions, filename, rel_pos, rel_orn, color = shape[:8]
             if type == p.GEOM_MESH:
                 filename = filename.decode('utf-8')
-                if (filename,(*dimensions)) not in self.visual_objects.keys():
+                if (filename, (*dimensions)) not in self.visual_objects.keys():
                     self.renderer.load_object(filename,
                                               transform_orn=rel_orn,
                                               transform_pos=rel_pos,
                                               input_kd=color[:3],
                                               scale=np.array(dimensions))
-                    self.visual_objects[(filename,(*dimensions))] = len(self.renderer.visual_objects) - 1
-                visual_objects.append(self.visual_objects[(filename,(*dimensions))])
+                    self.visual_objects[(filename, (*dimensions))
+                                        ] = len(self.renderer.visual_objects) - 1
+                visual_objects.append(
+                    self.visual_objects[(filename, (*dimensions))])
                 link_ids.append(link_id)
             elif type == p.GEOM_SPHERE:
-                filename = os.path.join(gibson2.assets_path, 'models/mjcf_primitives/sphere8.obj')
+                filename = os.path.join(
+                    gibson2.assets_path, 'models/mjcf_primitives/sphere8.obj')
                 self.renderer.load_object(
                     filename,
                     transform_orn=rel_orn,
@@ -412,7 +428,8 @@ class Simulator:
                 visual_objects.append(len(self.renderer.visual_objects) - 1)
                 link_ids.append(link_id)
             elif type == p.GEOM_CAPSULE or type == p.GEOM_CYLINDER:
-                filename = os.path.join(gibson2.assets_path, 'models/mjcf_primitives/cube.obj')
+                filename = os.path.join(
+                    gibson2.assets_path, 'models/mjcf_primitives/cube.obj')
                 self.renderer.load_object(
                     filename,
                     transform_orn=rel_orn,
@@ -422,7 +439,8 @@ class Simulator:
                 visual_objects.append(len(self.renderer.visual_objects) - 1)
                 link_ids.append(link_id)
             elif type == p.GEOM_BOX:
-                filename = os.path.join(gibson2.assets_path, 'models/mjcf_primitives/cube.obj')
+                filename = os.path.join(
+                    gibson2.assets_path, 'models/mjcf_primitives/cube.obj')
                 self.renderer.load_object(filename,
                                           transform_orn=rel_orn,
                                           transform_pos=rel_pos,
@@ -476,19 +494,37 @@ class Simulator:
         :param instance: Instance in the renderer
         """
         if isinstance(instance, Instance):
-            pos, orn = p.getBasePositionAndOrientation(instance.pybullet_uuid) # orn is in x,y,z,w
+            pos, orn = p.getBasePositionAndOrientation(
+                instance.pybullet_uuid)  # orn is in x,y,z,w
             instance.set_position(pos)
             instance.set_rotation(xyzw2wxyz(orn))
         elif isinstance(instance, InstanceGroup):
             poses_rot = []
             poses_trans = []
-
             for link_id in instance.link_ids:
                 if link_id == -1:
-                    pos, orn = p.getBasePositionAndOrientation(instance.pybullet_uuid)
+                    # pos and orn of the inertial frame of the base link,
+                    # instead of the base link frame
+                    pos, orn = p.getBasePositionAndOrientation(
+                        instance.pybullet_uuid)
+
+                    # Need to convert to the base link frame because that is
+                    # what our own renderer keeps track of
+                    # Based on pyullet docuementation:
+                    # urdfLinkFrame = comLinkFrame * localInertialFrame.inverse().
+                    _, _, _, inertial_pos, inertial_orn, _, _, _, _, _, _, _ = \
+                        p.getDynamicsInfo(instance.pybullet_uuid, link_id)
+                    inv_inertial_pos, inv_inertial_orn =\
+                        p.invertTransform(inertial_pos, inertial_orn)
+
+                    # Now pos and orn are converted to the base link frame
+                    pos, orn = p.multiplyTransforms(
+                        pos, orn, inv_inertial_pos, inv_inertial_orn)
                 else:
-                    _, _, _, _, pos, orn = p.getLinkState(instance.pybullet_uuid, link_id)
-                poses_rot.append(np.ascontiguousarray(quat2rotmat(xyzw2wxyz(orn))))
+                    _, _, _, _, pos, orn = p.getLinkState(
+                        instance.pybullet_uuid, link_id)
+                poses_rot.append(np.ascontiguousarray(
+                    quat2rotmat(xyzw2wxyz(orn))))
                 poses_trans.append(np.ascontiguousarray(xyz2mat(pos)))
 
             instance.poses_rot = poses_rot

test/scene_converter.py
@@ -18,10 +18,20 @@ import math
 
 
 config = parse_config(os.path.join(gibson2.root_path, '../test/test.yaml'))
+missing_models = set([
+    'exercise_equipment',
+    'curtain',
+    'garbage_compressor',
+    'staircase',
+    'closet',
+    'ac_engine',
+    'water_hearter',
+])
 
 def convert_scene(scene_name):
-    
-    scene_file = get_ig_scene_path(scene_name) + "/" + scene_name + "_orig.urdf"
+
+    scene_file = get_ig_scene_path(
+        scene_name) + "/" + scene_name + "_orig.urdf"
     scene_tree = ET.parse(scene_file)
     bbox_dir = os.path.join(get_ig_scene_path(scene_name), "bbox")
     os.makedirs(bbox_dir, exist_ok=True)
@@ -34,27 +44,21 @@ def convert_scene(scene_name):
         total = 0
         categories = []
         for c, obj in enumerate(all_objs):
-
             obj_category = obj["category"].lower()
-            if obj_category in ['stool', 'fence', 'table']:
-            # if obj_category not in ['piano', 'chair', 'door', 'sofa']:
-            # if obj_category not in ['door', 'window', 'cabinet']:
-            # if obj_category not in ['table']:
-            # if obj_category in ['table']:
-                # print("We don't have yet models of ", obj_category)
+            # print('obj_category:', obj_category)
+            if obj_category in missing_models:
+                print("We don't have yet models of ", obj_category)
                 continue
-            # obj_category = 'pool_table'
 
             total += 1
-            # if total > 1: 
-                # break
 
             link_name = obj_category + "_" + str(c)
 
             if obj_category not in categories:
                 categories += [obj_category]
 
-            link_el = ET.SubElement(scene_tree.getroot(), 'link', dict([("name", link_name), ("category", obj_category), ("model", "random")]))
+            link_el = ET.SubElement(scene_tree.getroot(), 'link', dict(
+                [("name", link_name), ("category", obj_category), ("model", "random")]))
             if obj['instance'] is not None:
                 link_el.set("random_group", str(obj['instance']))
 
@@ -62,18 +66,20 @@ def convert_scene(scene_name):
             bbox_x = np.linalg.norm(edge_x)
             edge_y = obj['edge_y']
             bbox_y = np.linalg.norm(edge_y)
-            print(bbox_x, bbox_y)
+            # print(bbox_x, bbox_y)
             z_bbox_coords = obj['z']
             bbox_z = (z_bbox_coords[1] - z_bbox_coords[0]) * 0.99
 
             # Ugly hack: Apparently the image had x-y swapped so we need to swap them also here
-            link_el.set("bounding_box", "{0:f} {1:f} {2:f}".format(bbox_y, bbox_x, bbox_z))
+            link_el.set("bounding_box", "{0:f} {1:f} {2:f}".format(
+                bbox_y, bbox_x, bbox_z))
 
             joint_name = "j_" + link_name
 
-            joint_el = ET.SubElement(scene_tree.getroot(), 'joint', dict([("name", joint_name)]))
+            joint_el = ET.SubElement(
+                scene_tree.getroot(), 'joint', dict([("name", joint_name)]))
 
-            print(obj['is_fixed'])
+            # print('is_fixed:', obj['is_fixed'])
             if obj['is_fixed'] == True:
                 joint_el.set("type", "fixed")
             else:
@@ -91,68 +97,73 @@ def convert_scene(scene_name):
             # edge_y= np.asarray(obj['edge_y'])
             # z= obj['z']
             # gen_cube_obj(center, edge_x, edge_y, z, bbox_file, is_color=True)
-            write_obj(*gen_rotated_obj(center, bbox_x,bbox_y,z_bbox_coords, yaw),bbox_file )
+            write_obj(*gen_rotated_obj(center, bbox_x, bbox_y,
+                                       z_bbox_coords, obj['theta']), bbox_file)
 
             # Ugly hack: Apparently the image had x-y swapped so we need to swap them also here
             origin = \
-                ET.SubElement(joint_el, 'origin', 
-                              dict([("xyz", "{0:f} {1:f} {2:f}".format(xy_coord[0], xy_coord[1], z_coord)), 
+                ET.SubElement(joint_el, 'origin',
+                              dict([("xyz", "{0:f} {1:f} {2:f}".format(xy_coord[0], xy_coord[1], z_coord)),
                                     ('rpy', "0 0 {0:f}".format(yaw))]))
 
-            child = ET.SubElement(joint_el, 'child', dict([("link", link_name) ]))
-            parent = ET.SubElement(joint_el, 'parent', dict([("link", "world") ]))  # We connect the embedded URDFs to the world
-            print(total)
+            child = ET.SubElement(
+                joint_el, 'child', dict([("link", link_name)]))
+            # We connect the embedded URDFs to the world
+            parent = ET.SubElement(
+                joint_el, 'parent', dict([("link", "world")]))
+            # print(total)
 
     scene_file_out = get_ig_scene_path(scene_name) + "/" + scene_name + ".urdf"
     scene_tree.write(scene_file_out)
-    print(categories)
+    print('all categories:', categories)
 
 
 def gen_rotated_obj(center, scale_x, scale_y, z, theta):
-    rot = np.array([[np.cos(theta),-np.sin(theta),0],
-                     [np.sin(theta),np.cos(theta),0],
-                     [0,0,1]])
+    rot = np.array([[np.cos(theta), -np.sin(theta), 0],
+                    [np.sin(theta), np.cos(theta), 0],
+                    [0, 0, 1]])
     vertices = []
-    a,b,c,d = get_coords([1,0], [0,1], [0,0])
-    for x,y in [a,b,d,c]:
-        vertices.append((x,y,z[1]))
-    for x,y in [a,b,d,c]:
-        vertices.append((x,y,z[0]))
+    a, b, c, d = get_coords([1, 0], [0, 1], [0, 0])
+    for x, y in [a, b, d, c]:
+        vertices.append((x, y, z[1]))
+    for x, y in [a, b, d, c]:
+        vertices.append((x, y, z[0]))
     vertices = np.array(vertices)
     vertices *= [scale_x, scale_y, 1.]
     vertices = vertices.dot(rot)
     vertices += [*center, 0]
-    faces = [(1,2,3), (2,4,3), (1,3,5),
-             (3,7,5), (1,5,2), (5,6,2),
-             (2,6,4), (6,8,4), (4,8,7),
-             (7,3,4), (6,7,8), (6,5,7),]
+    faces = [(1, 2, 3), (2, 4, 3), (1, 3, 5),
+             (3, 7, 5), (1, 5, 2), (5, 6, 2),
+             (2, 6, 4), (6, 8, 4), (4, 8, 7),
+             (7, 3, 4), (6, 7, 8), (6, 5, 7), ]
     faces = [(*f, -1) for f in faces]
     return vertices, faces
+
 
 def gen_cube_obj_raw(center, edge_x, edge_y, z):
     vertices = []
-    a,b,c,d = get_coords(edge_x, edge_y, center)
-    for x,y in [a,b,d,c]:
-        vertices.append((x,y,z[1]))
-    for x,y in [a,b,d,c]:
-        vertices.append((x,y,z[0]))
-    faces = [(1,2,3), (2,4,3), (1,3,5),
-             (3,7,5), (1,5,2), (5,6,2),
-             (2,6,4), (6,8,4), (4,8,7),
-             (7,3,4), (6,7,8), (6,5,7),]
+    a, b, c, d = get_coords(edge_x, edge_y, center)
+    for x, y in [a, b, d, c]:
+        vertices.append((x, y, z[1]))
+    for x, y in [a, b, d, c]:
+        vertices.append((x, y, z[0]))
+    faces = [(1, 2, 3), (2, 4, 3), (1, 3, 5),
+             (3, 7, 5), (1, 5, 2), (5, 6, 2),
+             (2, 6, 4), (6, 8, 4), (4, 8, 7),
+             (7, 3, 4), (6, 7, 8), (6, 5, 7), ]
     faces = [(*f, -1) for f in faces]
     return vertices, faces
 
 
-def write_obj(vertices,faces,file_path,is_color=True):
-    c=np.random.rand(3)
+def write_obj(vertices, faces, file_path, is_color=True):
+    c = np.random.rand(3)
     with open(file_path, 'w') as fp:
         for v in vertices:
             if is_color:
-                v1,v2,v3 = v
+                v1, v2, v3 = v
                 fp.write('v {} {} {} {} {} {}\n'.format(v1, v2, v3, *c))
             else:
-                v1,v2,v3 = v
+                v1, v2, v3 = v
                 fp.write('v {} {} {}\n'.format(v1, v2, v3))
         for f in faces:
             fp.write('f {} {} {}\n'.format(*f[:-1]))
@@ -165,19 +176,21 @@ def get_coords(edge_x, edge_y, center):
     x = np.array(edge_x) / 2.
     y = np.array(edge_y) / 2.
     return np.array([center - x - y,
-                    center + x - y,
-                    center + x + y,
-                    center - x + y])
-
+                     center + x - y,
+                     center + x + y,
+                     center - x + y])
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Convert from old json annotation into new urdf models.')
-    parser.add_argument('scene_names', metavar='s', type=str, nargs='+', help='The name of the scene to process')
+    parser = argparse.ArgumentParser(
+        description='Convert from old json annotation into new urdf models.')
+    parser.add_argument('scene_names', metavar='s', type=str,
+                        nargs='+', help='The name of the scene to process')
 
     args = parser.parse_args()
     for scene_name in args.scene_names:
         convert_scene(scene_name)
+
 
 if __name__ == "__main__":
     main()