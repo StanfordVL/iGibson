cb85a5423efceaa2b124aa6c875257d7fe2fce67
fxia22 <xf1280@gmail.com>
2020-09-18 15:01:24 -0700

    add turtlebot3

--- gibson2/core/physics/robot_locomotors.py
+++ gibson2/core/physics/robot_locomotors.py
@@ -444,6 +444,41 @@ class Turtlebot(LocomotorRobot):
         }
 
 
+class Turtlebot3(LocomotorRobot):
+    def __init__(self, config):
+        self.config = config
+        self.velocity = config.get("velocity", 0.5)
+        LocomotorRobot.__init__(self,
+                                "turtlebot3_description/turtlebot3_waffle_pi_for_open_manipulator.urdf",
+                                action_dim=2,
+                                scale=config.get("robot_scale", 1.0),
+                                is_discrete=config.get("is_discrete", False),
+                                control="velocity")
+
+    def set_up_continuous_action_space(self):
+        self.action_space = gym.spaces.Box(shape=(self.action_dim,),
+                                           low=-1.0,
+                                           high=1.0,
+                                           dtype=np.float32)
+        self.action_high = np.full(shape=self.action_dim, fill_value=self.velocity)
+        self.action_low = -self.action_high
+
+    def set_up_discrete_action_space(self):
+        self.action_list = [[self.velocity, self.velocity], [-self.velocity, -self.velocity],
+                            [self.velocity * 0.5, -self.velocity * 0.5],
+                            [-self.velocity * 0.5, self.velocity * 0.5], [0, 0]]
+        self.action_space = gym.spaces.Discrete(len(self.action_list))
+        self.setup_keys_to_action()
+
+    def setup_keys_to_action(self):
+        self.keys_to_action = {
+            (ord('w'),): 0,  # forward
+            (ord('s'),): 1,  # backward
+            (ord('d'),): 2,  # turn right
+            (ord('a'),): 3,  # turn left
+            (): 4  # stay still
+        }
+
 class Freight(LocomotorRobot):
     def __init__(self, config):
         self.config = config
