19475766a2266ee7d80ba6030250d1ee5e0fd82c
fxia22 <xf1280@gmail.com>
2020-10-04 15:41:28 -0700

    add new benchmarks

--- test/benchmark/benchmark_interactive_scene.py
+++ test/benchmark/benchmark_interactive_scene.py
@@ -2,22 +2,25 @@
 
 from gibson2.simulator import Simulator
 from gibson2.scenes.igibson_indoor_scene import InteractiveIndoorScene
-from gibson2.utils.utils import parse_config
 import os
 import gibson2
 import time
 import random
+import matplotlib.pyplot as plt
+from gibson2.utils.assets_utils import get_ig_assets_version
 
-def test_import_igsdf():
+def benchmark_scene(scene_name):
+    assets_version = get_ig_assets_version()
+    print('assets_version', assets_version)
     random.seed(0)
-    config = parse_config(os.path.join(gibson2.root_path, '../test/test.yaml'))
-    scene = InteractiveIndoorScene('Rs', texture_randomization=False)
+    scene = InteractiveIndoorScene(scene_name, texture_randomization=False)
     s = Simulator(mode='headless', image_width=512,
                   image_height=512, device_idx=0)
     s.import_ig_scene(scene)
 
     s.renderer.use_pbr(use_pbr=True, use_pbr_mapping=True)
-    for i in range(10):
+    fps = []
+    for i in range(500):
         # if i % 100 == 0:
         #     scene.randomize_texture()
         start = time.time()
@@ -25,13 +28,20 @@ def test_import_igsdf():
         end = time.time()
         print("Elapsed time: ", end - start)
         print("Frequency: ", 1 / (end - start))
-
+        fps.append(1 / (end - start))
     s.disconnect()
+
+    plt.hist(fps)
+    plt.xlabel('fps')
+    plt.title('Scene {} version {}'.format(scene_name, assets_version))
+    plt.savefig('scene_benchmark_{}.pdf'.format(scene_name))
     print("end")
 
 
 def main():
-    test_import_igsdf()
+    benchmark_scene('Rs')
+    benchmark_scene('Wainscott_0')
+
 
 if __name__ == "__main__":
     main()
