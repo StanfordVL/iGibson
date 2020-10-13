0552a700c61495ed715ef42d8eb97d54a4dbc10c
b0ku1 <bokuishen@gmail.com>
2020-10-13 12:33:28 -0700

    random scaling

--- gibson2/render/mesh_renderer/mesh_renderer_cpu.py
+++ gibson2/render/mesh_renderer/mesh_renderer_cpu.py
@@ -527,7 +527,7 @@ class RandomizedMaterial(Material):
         self.roughness_texture_id = self.random_instance['roughness']
         self.normal_texture_id = self.random_instance['normal']
         # self.transform_param = transform_param # x scale, y scale, rotation
-        scale = random.normal(loc=4, scale=1) # scaling by 4 is typically good
+        scale = np.random.normal(loc=4, scale=1) # scaling by 4 is typically good
         scale = max(scale, 2) # scaling should be at least 2.
         rotation = random.randint(0,3) * math.pi / 2.
         self.transform_param = [scale, scale, rotation]
