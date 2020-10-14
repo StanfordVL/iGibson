14d9be73b7a6df433b7745ad4e93e1af57552c38
fxia22 <xf1280@gmail.com>
2020-10-14 01:01:34 -0700

    use new light probes

--- examples/demo/mouse_interaction.py
+++ examples/demo/mouse_interaction.py
@@ -17,8 +17,9 @@ def test_import_igsdf():
     seeds = get_ig_scene_non_colliding_seeds('Rs')
     random.seed(seeds[0])
     config = parse_config(os.path.join(gibson2.root_path, '../test/test.yaml'))
-    hdr_texture = os.path.join(gibson2.ig_dataset_path, 'scenes', 'background', 'photo_studio_01_2k.hdr')
-    hdr_texture2 = os.path.join(gibson2.ig_dataset_path, 'scenes', 'background', 'Rs.hdr')
+    hdr_texture = os.path.join(gibson2.ig_dataset_path, 'scenes', 'Rs', 'lighting', 'probes', 'probe_02.hdr')
+    hdr_texture2 = os.path.join(gibson2.ig_dataset_path, 'scenes', 'Rs', 'lighting', 'probes', 'probe_03.hdr')
+    light_modulation_map_filename = os.path.join(gibson2.ig_dataset_path, 'scenes', 'Rs', 'layout', 'light_fusion_map.png')
     background_texture = os.path.join(gibson2.ig_dataset_path, 'scenes', 'background', 'urban_street_01.jpg')
 
     scene = InteractiveIndoorScene('Rs', texture_randomization=False, object_randomization=False)
@@ -26,8 +27,9 @@ def test_import_igsdf():
     settings = MeshRendererSettings(env_texture_filename=hdr_texture, 
                                     env_texture_filename2=hdr_texture2,
                                     env_texture_filename3=background_texture,
+                                    light_modulation_map_filename=light_modulation_map_filename,
                                     enable_shadow=True, msaa=True,
-                                    light_dimming_factor=1.2)
+                                    light_dimming_factor=1.0)
     s = Simulator(mode='iggui', image_width=960,
                   image_height=720, device_idx=0, rendering_settings=settings)
 
