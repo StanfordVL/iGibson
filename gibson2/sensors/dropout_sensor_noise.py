f51ce7053e64f9c14ea4812363d7e832146b2e51
Chengshu Li <chengshu@stanford.edu>
2020-11-19 11:26:06 -0800

    refactor Sensors out of Env

---
+++ gibson2/sensors/dropout_sensor_noise.py
@@ -0,0 +1,35 @@
+from gibson2.sensors.sensor_noise_base import BaseSensorNoise
+
+import numpy as np
+
+
+class DropoutSensorNoise(BaseSensorNoise):
+    def __init__(self, env):
+        super(DropoutSensorNoise, self).__init__(env)
+        self.noise_rate = 0.0
+        self.noise_value = 1.0
+
+    def set_noise_rate(self, noise_rate):
+        self.noise_rate = noise_rate
+
+    def set_noise_value(self, noise_value):
+        self.noise_value = noise_value
+
+    def add_noise(self, obs):
+        """
+        Add naive sensor dropout to perceptual sensor, such as RGBD and LiDAR scan
+        :param sensor_reading: raw sensor reading, range must be between [0.0, 1.0]
+        :param noise_rate: how much noise to inject, 0.05 means 5% of the data will be replaced with noise_value
+        :param noise_value: noise_value to overwrite raw sensor reading
+        :return: sensor reading corrupted with noise
+        """
+        if self.noise_rate <= 0.0:
+            return obs
+
+        assert len(obs[(obs < 0.0) | (obs > 1.0)]) == 0,\
+            'sensor reading has to be between [0.0, 1.0]'
+
+        valid_mask = np.random.choice(2, obs.shape, p=[
+                                      self.noise_rate, 1.0 - self.noise_rate])
+        obs[valid_mask == 0] = self.noise_value
+        return obs
