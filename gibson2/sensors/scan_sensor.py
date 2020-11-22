dfab8ab6edd243903e1d9773237f2be5de87ec95
fxia22 <xf1280@gmail.com>
2020-11-22 15:17:58 -0800

    occupancy grid change to 3 states

--- gibson2/sensors/scan_sensor.py
+++ gibson2/sensors/scan_sensor.py
@@ -74,25 +74,31 @@ class ScanSensor(BaseSensor):
         # flip y axis
         scan_local[:, 1] *= -1
 
-        occupancy_grid = np.zeros(
+        occupancy_grid = np.ones(
             (self.grid_resolution, self.grid_resolution)).astype(np.uint8)
         scan_local_in_map = scan_local / self.occupancy_range * \
                             self.grid_resolution + (self.grid_resolution / 2)
         scan_local_in_map = scan_local_in_map.reshape(
             (1, -1, 1, 2)).astype(np.int32)
-
+        for i in range(scan_local_in_map.shape[1]):
+            cv2.circle(img=occupancy_grid,
+                       center=(scan_local_in_map[0,i,0,0],
+                               scan_local_in_map[0,i,0,1]),
+                       radius=2,
+                       color=0,
+                       thickness=-1)
         cv2.fillPoly(img=occupancy_grid,
                      pts=scan_local_in_map,
-                     color=True,
+                     color=2,
                      lineType=1)
         cv2.circle(img=occupancy_grid,
                    center=(self.grid_resolution // 2,
                            self.grid_resolution // 2),
                    radius=int(self.robot_footprint_radius_in_map),
-                   color=1,
+                   color=2,
                    thickness=-1)
 
-        return occupancy_grid[:, :, None].astype(np.float32)
+        return occupancy_grid[:, :, None].astype(np.float32) / 2.0
 
     def get_obs(self, env):
         """
