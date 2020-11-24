3cd91899089086f651d819113cccae6791b020e1
fxia22 <xf1280@gmail.com>
2020-11-24 12:13:22 -0800

    minor fix

---
+++ gibson2/render/mesh_renderer/mesh_renderer_settings.py
@@ -0,0 +1,57 @@
+import os
+import gibson2
+import platform
+
+class MeshRendererSettings(object):
+    def __init__(self,
+                 use_fisheye=False,
+                 msaa=False,
+                 enable_shadow=False,
+                 enable_pbr=True,
+                 env_texture_filename=os.path.join(gibson2.ig_dataset_path, 'scenes', 'background',
+                                                   'photo_studio_01_2k.hdr'),
+                 env_texture_filename2=os.path.join(gibson2.ig_dataset_path, 'scenes', 'background',
+                                                    'photo_studio_01_2k.hdr'),
+                 env_texture_filename3=os.path.join(gibson2.ig_dataset_path, 'scenes', 'background',
+                                                    'photo_studio_01_2k.hdr'),
+                 light_modulation_map_filename='',
+                 optimized=False,
+                 skybox_size=20.,
+                 light_dimming_factor=1.0,
+                 fullscreen=False,
+                 glfw_gl_version=None,
+                 texture_scale=1.0,
+                 hide_robot=True,
+                 ):
+        self.use_fisheye = use_fisheye
+        self.msaa = msaa
+        self.enable_shadow = enable_shadow
+        self.env_texture_filename = env_texture_filename
+        self.env_texture_filename2 = env_texture_filename2
+        self.env_texture_filename3 = env_texture_filename3
+        self.optimized = optimized
+        self.skybox_size = skybox_size
+        self.light_modulation_map_filename = light_modulation_map_filename
+        self.light_dimming_factor = light_dimming_factor
+        self.enable_pbr = enable_pbr
+        self.fullscreen = fullscreen
+        self.texture_scale = texture_scale
+        self.hide_robot=hide_robot
+
+        if glfw_gl_version is not None:
+            self.glfw_gl_version = glfw_gl_version
+        else:
+            if platform.system() == 'Darwin':
+                self.glfw_gl_version = [4, 1]
+            else:
+                self.glfw_gl_version = [4, 5]
+
+    def get_fastest(self):
+        self.msaa = False
+        self.enable_shadow = False
+        return self
+
+    def get_best(self):
+        self.msaa = True
+        self.enable_shadow = True
+        return self
