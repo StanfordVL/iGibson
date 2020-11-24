01b2f3b6015a767796b6d2c6e1bd6472002b204e
fxia22 <xf1280@gmail.com>
2020-11-23 23:39:16 -0800

    fix and clean up rendering to tensor

--- gibson2/render/mesh_renderer/mesh_renderer_tensor.py
+++ gibson2/render/mesh_renderer/mesh_renderer_tensor.py
@@ -22,8 +22,10 @@ try:
                 self.normal_tensor = torch.cuda.ByteTensor(height, width, 4).cuda()
                 self.seg_tensor = torch.cuda.ByteTensor(height, width, 4).cuda()
                 self.pc_tensor = torch.cuda.FloatTensor(height, width, 4).cuda()
+                self.optical_flow_tensor = torch.cuda.FloatTensor(height, width, 4).cuda()
+                self.scene_flow_tensor = torch.cuda.FloatTensor(height, width, 4).cuda()
 
-        def readbuffer_to_tensor(self, modes=('rgb', 'normal', 'seg', '3d')):
+        def readbuffer_to_tensor(self, modes=('rgb', 'normal', 'seg', '3d', 'optical_flow', 'scene_flow')):
             results = []
 
             # single mode
@@ -32,7 +34,7 @@ try:
 
             with torch.cuda.device(self.cuda_idx):
                 for mode in modes:
-                    if mode not in ['rgb', 'normal', 'seg', '3d']:
+                    if mode not in ['rgb', 'normal', 'seg', '3d', 'optical_flow', 'scene_flow']:
                         raise Exception('unknown rendering mode: {}'.format(mode))
                     if mode == 'rgb':
                         self.r.map_tensor(int(self.color_tex_rgb), int(self.width), int(self.height),
@@ -50,45 +52,28 @@ try:
                         self.r.map_tensor_float(int(self.color_tex_3d), int(self.width), int(self.height),
                                                 self.pc_tensor.data_ptr())
                         results.append(self.pc_tensor.clone())
+                    elif mode == 'optical_flow':
+                        self.r.map_tensor_float(int(self.color_tex_optical_flow), int(self.width), int(self.height),
+                                                self.optical_flow_tensor.data_ptr())
+                        results.append(self.optical_flow_tensor.clone())
+                    elif mode == 'scene_flow':
+                        self.r.map_tensor_float(int(self.color_tex_scene_flow), int(self.width), int(self.height),
+                                                self.scene_flow_tensor.data_ptr())
+                        results.append(self.scene_flow_tensor.clone())
+
             return results
 
-        def render(self, modes=('rgb', 'normal', 'seg', '3d'), hidden=()):
+        def render(self, modes=('rgb', 'normal', 'seg', '3d', 'optical_flow', 'scene_flow'), hidden=(),
+                   return_buffer=True, render_shadow_pass=True):
             """
             A function to render all the instances in the renderer and read the output from framebuffer into pytorch tensor.
 
-            :param modes: it should be a tuple consisting of a subset of ('rgb', 'normal', 'seg', '3d').
+            :param modes: it should be a tuple consisting of a subset of ('rgb', 'normal', 'seg', '3d', 'optical_flow', 'scene_flow').
             :param hidden: Hidden instances to skip. When rendering from a robot's perspective, it's own body can be
                 hidden
-
             """
-
-            if self.enable_shadow:
-                # shadow pass
-                V = np.copy(self.V)
-                self.V = np.copy(self.lightV)
-                self.r.render_tensor_pre(0, 0, self.fbo)
-
-                for instance in self.instances:
-                    if not instance in hidden:
-                        instance.render()
-
-                self.r.render_tensor_post()
-                self.r.readbuffer_meshrenderer_shadow_depth(self.width, self.height, self.fbo, self.depth_tex_shadow)
-                self.V = np.copy(V)
-
-            if self.msaa:
-                self.r.render_tensor_pre(1, self.fbo_ms, self.fbo)
-            else:
-                self.r.render_tensor_pre(0, 0, self.fbo)
-
-            for instance in self.instances:
-                if not instance in hidden:
-                    instance.render()
-
-            self.r.render_tensor_post()
-            if self.msaa:
-                self.r.blit_buffer(self.width, self.height, self.fbo_ms, self.fbo)
-
+            super(MeshRendererG2G, self).render(modes=modes, hidden=hidden, return_buffer=False,
+                                      render_shadow_pass=render_shadow_pass)
             return self.readbuffer_to_tensor(modes)
 except:
     print("torch is not available, falling back to rendering to memory(instead of tensor)")
