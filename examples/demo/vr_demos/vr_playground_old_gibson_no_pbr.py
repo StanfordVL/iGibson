7ed5b5fdefdd0d158c34fdec85b32cef20b0c0f9
kenzomenzo <kent@vainionet.com.com>
2020-12-04 14:43:34 +0000

    Cleaned up VR API, simplified demos and added feature to allow user to change height

--- examples/demo/vr_demos/vr_playground_old_gibson_no_pbr.py
+++ examples/demo/vr_demos/vr_playground_old_gibson_no_pbr.py
@@ -1,12 +1,11 @@
-""" VR playground containing various objects and VR options that can be toggled
-to experiment with the VR experience in iGibson. This playground operates in
+""" VR playground containing various objects. This playground operates in
 the Placida scene, which is from the set of old iGibson environments and does not use PBR.
 
 Important: VR functionality and where to find it:
 
 1) Most VR functions can be found in the gibson2/simulator.py
 2) VR utility functions are found in gibson2/utils/vr_utils.py
-3) The VR renderer can be found in gibson2/render/mesh_renderer.py
+3) The VR renderer and VR settings can be found in gibson2/render/mesh_renderer.py
 4) The underlying VR C++ code can be found in vr_mesh_render.h and .cpp in gibson2/render/cpp
 """
 
@@ -15,63 +14,32 @@ import os
 import pybullet as p
 
 from gibson2.render.mesh_renderer.mesh_renderer_cpu import MeshRendererSettings
+from gibson2.render.mesh_renderer.mesh_renderer_vr import VrSettings
 from gibson2.scenes.gibson_indoor_scene import StaticIndoorScene
 from gibson2.objects.articulated_object import ArticulatedObject
-from gibson2.objects.vr_objects import VrBody, VrHand
+from gibson2.objects.vr_objects import VrBody, VrHand, VrGazeMarker
 from gibson2.objects.visual_marker import VisualMarker
 from gibson2.objects.ycb_object import YCBObject
 from gibson2.simulator import Simulator
-from gibson2.utils.vr_utils import move_player
 from gibson2 import assets_path
+
 sample_urdf_folder = os.path.join(assets_path, 'models', 'sample_urdfs')
 
-# Playground configuration: edit this to change functionality
-optimize = True
-# Toggles fullscreen companion window
-fullscreen = False
-# Toggles SRAnipal eye tracking
-use_eye_tracking = True
-# Enables the VR collision body
-enable_vr_body = True
-# Toggles movement with the touchpad (to move outside of play area)
-touchpad_movement = True
-# Set to one of hmd, right_controller or left_controller to move relative to that device
-relative_movement_device = 'hmd'
-# Movement speed for touchpad-based movement
-movement_speed = 0.02
-# Whether we should hide a mustard bottle when the menu button is presed
-hide_mustard_on_press = True
-
-# Initialize simulator with specific rendering settings
-s = Simulator(mode='vr', rendering_settings=MeshRendererSettings(optimized=optimize, fullscreen=fullscreen, enable_pbr=False),
-            vr_eye_tracking=use_eye_tracking, vr_mode=True)
+s = Simulator(mode='vr', 
+            rendering_settings=MeshRendererSettings(optimized=True, fullscreen=False, enable_pbr=False), 
+            vr_settings=VrSettings())
 scene = StaticIndoorScene('Placida')
 s.import_scene(scene)
 
-# Player body is represented by a translucent blue cylinder
-if enable_vr_body:
-    vr_body = VrBody(s)
-    s.import_object(vr_body)
-    vr_body.init_body()
+# VR objects automatically import themselves into simulator and perform setup
+# use_constraints allows the body and hands to be controlled by PyBullet's constraint system
+# This is turned off during full-state data replay
+vr_body = VrBody(s, use_constraints=True)
+r_hand = VrHand(s, hand='right', use_constraints=True)
+l_hand = VrHand(s, hand='left', use_constraints=True)
+gaze_marker = VrGazeMarker(s)
 
-# The hand can either be 'right' or 'left'
-# It has enough friction to pick up the basket and the mustard bottles
-r_hand = VrHand(s, hand='right')
-s.import_object(r_hand, use_pbr=False, use_pbr_mapping=False, shadow_caster=True)
-# This sets the hand constraints so it can move with the VR controller
-r_hand.hand_setup()
-
-l_hand = VrHand(s, hand='left')
-s.import_object(l_hand, use_pbr=False, use_pbr_mapping=False, shadow_caster=True)
-# This sets the hand constraints so it can move with the VR controller
-l_hand.hand_setup()
-
-if use_eye_tracking:
-    # Eye tracking visual marker - a red marker appears in the scene to indicate gaze direction
-    gaze_marker = VisualMarker(radius=0.03)
-    s.import_object(gaze_marker)
-    gaze_marker.set_position([0,0,1.5])
-
+# Import objects to interact with
 basket_path = os.path.join(sample_urdf_folder, 'object_ZU6u5fvE8Z1.urdf')
 basket = ArticulatedObject(basket_path)
 s.import_object(basket)
@@ -88,75 +56,19 @@ for i in range(len(mass_list)):
     mustard.set_position([mustard_start[0], mustard_start[1] - i * 0.2, mustard_start[2]])
     p.changeDynamics(mustard.body_id, -1, mass=mass_list[i])
 
-if optimize:
-    s.optimize_vertex_and_texture()
+s.optimize_vertex_and_texture()
 
-# Start user close to counter for interaction
-s.set_vr_offset([-0.5, 0.0, 0])
-
-# State of mustard hiding, toggled by a menu press
-hide_mustard = False
+# Since vr_height_offset is set, we will use the VR HMD true height plus this offset instead of the third entry of the start pos
+s.set_vr_start_pos([0.5, 0, 0], vr_height_offset=0)
 
 # Main simulation loop
 while True:
-    # Demonstrates how to call VR events - replace pass with custom logic
-    # See pollVREvents description in simulator for full list of events
-    event_list = s.poll_vr_events()
-    for event in event_list:
-        device_type, event_type = event
-        if device_type == 'right_controller':
-            if event_type == 'menu_press' and hide_mustard_on_press:
-                # Toggle mustard hidden state
-                hide_mustard = not hide_mustard
-                s.set_hidden_state(mustard_list[2], hide=hide_mustard)
-
-        # Hands can be reset by pressing the grip on the corresponding controller
-        if event_type == 'grip_press':
-            if device_type == 'left_controller':
-                l_hand.reset_hand_transform()
-            else:
-                r_hand.reset_hand_transform()
-
-    # Step the simulator - this needs to be done every frame to actually run the simulation
     s.step()
 
-    # VR device data
-    hmd_is_valid, hmd_trans, hmd_rot = s.get_data_for_vr_device('hmd')
-    l_is_valid, l_trans, l_rot = s.get_data_for_vr_device('left_controller')
-    r_is_valid, r_trans, r_rot = s.get_data_for_vr_device('right_controller')
-
-    # VR button data
-    l_trig, l_touch_x, l_touch_y = s.get_button_data_for_controller('left_controller')
-    r_trig, r_touch_x, r_touch_y = s.get_button_data_for_controller('right_controller')
-
-    # VR eye tracking data
-    if use_eye_tracking:
-        is_eye_data_valid, origin, dir, left_pupil_diameter, right_pupil_diameter = s.get_eye_tracking_data()
-        if is_eye_data_valid:
-            # Move gaze marker based on eye tracking data
-            updated_marker_pos = [origin[0] + dir[0], origin[1] + dir[1], origin[2] + dir[2]]
-            gaze_marker.set_position(updated_marker_pos)
-
-    if r_is_valid:
-        r_hand.move(r_trans, r_rot)
-        r_hand.set_close_fraction(r_trig)
-
-        # Right hand used to control movement
-        # Move VR system based on device coordinate system and touchpad press location
-        move_player(s, r_touch_x, r_touch_y, movement_speed, relative_movement_device)
-
-        # Trigger haptic pulse on right touchpad, modulated by trigger close fraction
-        # Close the trigger to create a stronger pulse
-        # Note: open trigger has closed fraction of 0.05 when open, so cutoff haptic input under 0.1
-        # to avoid constant rumbling
-        s.trigger_haptic_pulse('right_controller', r_trig if r_trig > 0.1 else 0)
-
-    if l_is_valid:
-        l_hand.move(l_trans, l_rot)
-        l_hand.set_close_fraction(l_trig)
-        s.trigger_haptic_pulse('left_controller', l_trig if l_trig > 0.1 else 0)
-
-    if enable_vr_body:
-        vr_body.update_body()
+    # Update VR objects
+    gaze_marker.update_marker()
+    r_hand.update_hand()
+    l_hand.update_hand()
+    vr_body.update_body()
 
 s.disconnect()
--- gibson2/objects/vr_objects.py
+++ gibson2/objects/vr_objects.py
@@ -4,8 +4,9 @@ import pybullet as p
 
 from gibson2 import assets_path
 from gibson2.objects.articulated_object import ArticulatedObject
+from gibson2.objects.visual_marker import VisualMarker
 from gibson2.utils.utils import multQuatLists
-from gibson2.utils.vr_utils import translate_vr_position_by_vecs
+from gibson2.utils.vr_utils import move_player, translate_vr_position_by_vecs
 
 
 class VrBody(ArticulatedObject):
@@ -14,24 +15,26 @@ class VrBody(ArticulatedObject):
     them from moving through physical objects and wall, as well
     as other VR users.
     """
-    def __init__(self, s):
+    def __init__(self, s, use_constraints=True):
         self.vr_body_fpath = os.path.join(assets_path, 'models', 'vr_body', 'vr_body.urdf')
-        super(VrBody, self).__init__(filename=self.vr_body_fpath, scale=1)
         self.sim = s
+        self.use_constraints = use_constraints
+        super(VrBody, self).__init__(filename=self.vr_body_fpath, scale=1)
         self.first_frame = True
         # Start body far above the scene so it doesn't interfere with physics
         self.start_pos = [0, 0, 150]
+        self.sim.import_object(self, use_pbr=False, use_pbr_mapping=False, shadow_caster=True)
+        self.init_body()
 
-    # TIMELINE: Call this after loading the VR body into the simulator
-    def init_body(self, use_constraints=True):
+    def init_body(self):
         """
-        Initialize VR body to start in a specific location.
+        Initializes VR body to start in a specific location.
         use_contraints specifies whether we want to move the VR body with
         constraints. This is True by default, but we set it to false
         when doing state replay, so constraints do not interfere with the replay.
         """
         self.set_position(self.start_pos)
-        if use_constraints:
+        if self.use_constraints:
             self.movement_cid = p.createConstraint(self.body_id, -1, -1, -1, p.JOINT_FIXED, 
                                                 [0, 0, 0], [0, 0, 0], self.start_pos)
         self.set_body_collision_filters()
@@ -50,7 +53,6 @@ class VrBody(ArticulatedObject):
                 for floor_link_idx in floor_link_idxs:
                     p.setCollisionFilterPair(self.body_id, f_id, body_link_idx, floor_link_idx, 0)
     
-    # TIMELINE: Call this after all other VR movement has been calculated in main while loop
     def update_body(self):
         """
         Updates VrBody to new position and rotation, via constraints.
@@ -69,10 +71,8 @@ class VrBody(ArticulatedObject):
 
             hmd_x, hmd_y, hmd_z = p.getEulerFromQuaternion(hmd_rot)
             right, _, forward = self.sim.get_device_coordinate_system('hmd')
-            print("hmd z: {}".format(hmd_z))
-            print("Forward: {}".format(forward))
-            print("------------------------------")
 
+            # TODO: Add a system that limits rotation when HMD has large X (or Y?) angle offset
             new_body_rot = p.getQuaternionFromEuler([0, 0, hmd_z])
 
             # Update body transform constraint
@@ -104,11 +104,14 @@ class VrHand(ArticulatedObject):
     """
 
     # VR hand can be one of three types - no_pbr (diffuse white/grey color), skin or metal
-    def __init__(self, sim, hand='right', tex_type='no_pbr'):
+    def __init__(self, s, hand='right', tex_type='no_pbr', use_constraints=True):
         # We store a reference to the simulator so that VR data can be acquired under the hood
-        self.sim = sim
+        self.sim = s
+        self.vr_settings = self.sim.vr_settings
         self.vr_hand_folder = os.path.join(assets_path, 'models', 'vr_hand')
         self.hand = hand
+        self.tex_type = tex_type
+        self.use_constraints = use_constraints
         if self.hand not in ['left', 'right']:
             print('ERROR: hand parameter must either be left or right!')
             return
@@ -138,8 +141,15 @@ class VrHand(ArticulatedObject):
         # Closed positions for all joints
         self.close_pos = [0, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 1.2, 0.5, 0.5, 0.5, 0.8, 0.8, 0.8]
 
+        # Import hand and setup
+        if tex_type == 'no_pbr':
+            self.sim.import_object(self, use_pbr=False, use_pbr_mapping=False, shadow_caster=True)
+        else:
+            self.sim.import_object(self, use_pbr=True, use_pbr_mapping=True, shadow_caster=True)
+        self.hand_setup()
+
     def hand_setup(self):
-        """Call after importing the hand. This sets the hand constraints and starting position"""
+        """Called after hand is imported. This sets the hand constraints and starting position"""
         # Set the hand to z=100 so it won't interfere with physics upon loading
         x_coord = 10 if self.hand == 'right' else -10
         start_pos = [x_coord, 0, 100]
@@ -153,32 +163,42 @@ class VrHand(ArticulatedObject):
             p.setJointMotorControl2(self.body_id, jointIndex, p.POSITION_CONTROL, targetPosition=open_pos, force=500)
         p.changeDynamics(self.body_id, -1, mass=0.2, lateralFriction=2)
         # Create constraint that can be used to move the hand
-        self.movement_cid = p.createConstraint(self.body_id, -1, -1, -1, p.JOINT_FIXED, [0, 0, 0], [0, 0, 0], start_pos)
+        if self.use_constraints:
+            self.movement_cid = p.createConstraint(self.body_id, -1, -1, -1, p.JOINT_FIXED, [0, 0, 0], [0, 0, 0], start_pos)
 
-    def reset_hand_transform(self):
+    # TIMELINE: Call after step in main while loop
+    def update_hand(self):
         """
-        Resets the transform of the VR hand by setting its position and orientation to that of the VR controller.
-        The hand's transform will only be set if the controller data is valid.
+        Updates position and close fraction of hand, and also moves player.
         """
         is_valid, trans, rot = self.sim.get_data_for_vr_device(self.vr_device)
+        trig_frac, touch_x, touch_y = self.sim.get_button_data_for_controller(self.vr_device)
         if is_valid:
-            self.set_position(trans)
-            # Apply base rotation first so the virtual controller is properly aligned with the real controller
-            final_rot = multQuatLists(rot, self.base_rot)
-            self.set_orientation(final_rot)
-            # We also need to update the hand's constraint to its new location
-            p.changeConstraint(self.movement_cid, trans, final_rot, maxForce=2000)
+            # Reset the hand if the grip has been pressed
+            if self.sim.query_vr_event(self.vr_device, 'grip_press'):
+                self.set_position(trans)
+                # Apply base rotation first so the virtual controller is properly aligned with the real controller
+                final_rot = multQuatLists(rot, self.base_rot)
+                self.set_orientation(final_rot)
 
-    # Close frac of 1 indicates fully closed joint, and close frac of 0 indicates fully open joint
-    # Joints move smoothly between their values in self.open_pos and self.close_pos
-    def set_close_fraction(self, close_frac):
-        for jointIndex in range(p.getNumJoints(self.body_id)):
-            open_pos = self.open_pos[jointIndex]
-            close_pos = self.close_pos[jointIndex]
-            interp_frac = (close_pos - open_pos) * close_frac
-            target_pos = open_pos + interp_frac
-            p.setJointMotorControl2(self.body_id, jointIndex, p.POSITION_CONTROL, targetPosition=target_pos, force=2000)
+            # Move the vr offset up/down if menu button is pressed - this can be used
+            # to adjust user height in the VR experience
+            if self.sim.query_vr_event(self.vr_device, 'menu_press'):
+                # Right menu button moves up, left menu button moves down
+                vr_z_offset = 0.01 if self.hand == 'right' else -0.01
+                curr_offset = self.sim.get_vr_offset()
+                self.sim.set_vr_offset([curr_offset[0], curr_offset[1], curr_offset[2] + vr_z_offset])
 
+            self.move(trans, rot)
+            self.set_close_fraction(trig_frac)
+
+            if self.vr_settings.touchpad_movement and self.hand == self.vr_settings.movement_controller:
+                move_player(self.sim, touch_x, touch_y, self.vr_settings.movement_speed, self.vr_settings.relative_movement_device)
+
+            # TODO: Detect collisions and add haptic rumble here - larger rumble for greater mass?
+            self.sim.trigger_haptic_pulse(self.vr_device, trig_frac if trig_frac > 0.1 else 0)
+
+    # Note: This function can be called manually during data replay
     def move(self, trans, rot):
         # If the hand is more than 2 meters away from the target, it will not move
         # We have a reset button to deal with this case, and we don't want to disturb the physics by trying to reconnect
@@ -189,3 +209,41 @@ class VrHand(ArticulatedObject):
         if dist_to_dest < 2.0:
             final_rot = multQuatLists(rot, self.base_rot)
             p.changeConstraint(self.movement_cid, trans, final_rot, maxForce=2000)
+
+    # Note: This function can be called manually during data replay
+    def set_close_fraction(self, close_frac):
+        """
+        Sets close fraction of hands. Close frac of 1 indicates fully closed joint, 
+        and close frac of 0 indicates fully open joint. Joints move smoothly between 
+        their values in self.open_pos and self.close_pos.
+        """
+        for jointIndex in range(p.getNumJoints(self.body_id)):
+            open_pos = self.open_pos[jointIndex]
+            close_pos = self.close_pos[jointIndex]
+            interp_frac = (close_pos - open_pos) * close_frac
+            target_pos = open_pos + interp_frac
+            p.setJointMotorControl2(self.body_id, jointIndex, p.POSITION_CONTROL, targetPosition=target_pos, force=2000)
+
+
+class VrGazeMarker(VisualMarker):
+    """
+    Represents the marker used for VR gaze tracking
+    """
+    def __init__(self, s):
+        # We store a reference to the simulator so that VR data can be acquired under the hood
+        self.sim = s
+        super(VrGazeMarker, self).__init__(visual_shape=p.GEOM_SPHERE, radius=0.02)
+        s.import_object(self)
+        # Set high above scene initially
+        self.set_position([0, 0, 200])
+
+    def update_marker(self):
+        if not self.sim.vr_settings.eye_tracking:
+            raise RuntimeError('ERROR: Unable to set vr gaze marker when eye tracking is not enabled.')
+
+        is_eye_data_valid, origin, dir, left_pupil_diameter, right_pupil_diameter = self.sim.get_eye_tracking_data()
+        if is_eye_data_valid:
+            updated_marker_pos = [origin[0] + dir[0], origin[1] + dir[1], origin[2] + dir[2]]
+            self.set_position(updated_marker_pos)
+
+
