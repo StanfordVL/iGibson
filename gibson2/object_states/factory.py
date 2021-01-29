e75867676b4fbceef5f63a7920aa38dc9816b1cf
Cem Gokmen <cgokmen@stanford.edu>
2021-01-29 11:59:24 -0800

    Refactor states to new architecture.
    Here, we drop the offline mode support in individual states & redesign the state interface, which will be used to support `properties` too starting in upcoming changes.

--- gibson2/object_states/factory.py
+++ gibson2/object_states/factory.py
@@ -2,14 +2,22 @@ from gibson2.object_states.pose import Pose
 from gibson2.object_states.aabb import AABB
 from gibson2.object_states.contact_bodies import ContactBodies
 from gibson2.object_states.dummy_state import DummyState
+from gibson2.object_states.temperature import Temperature
+
+STATE_NAME_TO_CLASS_MAPPING = {
+    'pose': Pose,
+    'aabb': AABB,
+    'contact_bodies': ContactBodies,
+    'temperature': Temperature,
+}
 
 
 def get_object_state_instance(state_name, obj, online=True):
-    if state_name == 'pose':
-        return Pose(obj, online) if online else DummyState(obj, online)
-    elif state_name == 'aabb':
-        return AABB(obj, online) if online else DummyState(obj, online)
-    elif state_name == 'contact_bodies':
-        return ContactBodies(obj, online) if online else DummyState(obj, online)
-    else:
+    if state_name not in STATE_NAME_TO_CLASS_MAPPING:
         assert False, 'unknown state name: {}'.format(state_name)
+
+    if not online:
+        return DummyState(obj)
+
+    state_class = STATE_NAME_TO_CLASS_MAPPING[state_name]
+    return state_class(obj)
