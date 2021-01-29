17d9337fe92b5aaae5d75ff225702e3fde203cc0
Cem Gokmen <cgokmen@stanford.edu>
2021-01-29 11:29:11 -0800

    Add DummyState class for use in offline mode.
    Currently each state class separately implements support for keeping values in memory in offline mode. This change centralizes that behavior to a single class which will then be used instead of the real classes in the object's state register. The individual states' offline behavior support will be removed in the upcoming refactor of states.

--- gibson2/object_states/factory.py
+++ gibson2/object_states/factory.py
@@ -1,14 +1,15 @@
 from gibson2.object_states.pose import Pose
 from gibson2.object_states.aabb import AABB
 from gibson2.object_states.contact_bodies import ContactBodies
+from gibson2.object_states.dummy_state import DummyState
 
 
 def get_object_state_instance(state_name, obj, online=True):
     if state_name == 'pose':
-        return Pose(obj, online)
+        return Pose(obj, online) if online else DummyState(obj, online)
     elif state_name == 'aabb':
-        return AABB(obj, online)
+        return AABB(obj, online) if online else DummyState(obj, online)
     elif state_name == 'contact_bodies':
-        return ContactBodies(obj, online)
+        return ContactBodies(obj, online) if online else DummyState(obj, online)
     else:
         assert False, 'unknown state name: {}'.format(state_name)
