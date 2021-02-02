914aedd46dd8e192b52d524da77d7a2d8a7a7d97
Cem Gokmen <cgokmen@stanford.edu>
2021-02-01 18:00:19 -0800

    Remove calls to non-standard object methods in Pose.

--- gibson2/object_states/pose.py
+++ gibson2/object_states/pose.py
@@ -5,9 +5,11 @@ import numpy as np
 class Pose(AbsoluteObjectState):
 
     def update(self, simulator):
-        pos, orn = self.obj.get_position_orientation()
+        pos = self.obj.get_position()
+        orn = self.obj.get_orientation()
         self.value = (np.array(pos), np.array(orn))
 
     def set_value(self, new_value):
         pos, orn = new_value
-        self.obj.set_position_orientation(pos, orn)
+        self.obj.set_position(pos)
+        self.obj.set_orientation(orn)
