f73cfc1dc7fca459e86cc89e9fcd9dbd277fb606
Cem Gokmen <cgokmen@stanford.edu>
2021-02-11 01:19:17 -0800

    Add heat transfer logic & demo.

---
+++ gibson2/examples/demo/temperature_example.py
@@ -0,0 +1,45 @@
+import os
+
+import gibson2
+import numpy as np
+
+from gibson2.objects.articulated_object import URDFObject
+from gibson2.scenes.empty_scene import EmptyScene
+from gibson2.simulator import Simulator
+from gibson2.utils.assets_utils import download_assets
+
+download_assets()
+
+
+def main():
+    s = Simulator(mode='gui')
+
+    try:
+        scene = EmptyScene()
+        s.import_scene(scene)
+
+        stove_dir = os.path.join(
+            gibson2.ig_dataset_path, 'objects/stove/101930/')
+        stove_urdf = os.path.join(stove_dir, "101930.urdf")
+        saucepan_dir = os.path.join(
+            gibson2.ig_dataset_path, 'objects/saucepan/38_2/')
+        saucepan_urdf = os.path.join(saucepan_dir, "38_2.urdf")
+
+        stove = URDFObject(stove_urdf, name="stove", model_path=stove_dir)
+        s.import_object(stove)
+        stove.set_position([0, 0, 0.76])
+
+        saucepan = URDFObject(saucepan_urdf, name="saucepan", model_path=saucepan_dir, scale=np.array([0.5, 0.5, 0.5]))
+        s.import_object(saucepan)
+        saucepan.set_position([-0.2, -0.2, 1.7])
+
+        # Run simulation for 1000 steps
+        while True:
+            s.step()
+            print("Saucepan Temperature: ", saucepan.states['temperature'].get_value())
+    finally:
+        s.disconnect()
+
+
+if __name__ == "__main__":
+    main()
--- gibson2/object_states/heat_source.py
+++ gibson2/object_states/heat_source.py
@@ -1,5 +1,9 @@
+from gibson2.external.pybullet_tools.utils import link_from_name, get_link_state
 from gibson2.object_states.object_state_base import CachingEnabledObjectState
 from gibson2.object_states.utils import get_aabb_center
+
+# The name of the heating element link inside URDF files.
+HEATING_ELEMENT_LINK_NAME = "heating_element"
 
 
 class HeatSource(CachingEnabledObjectState):
@@ -17,9 +21,14 @@ class HeatSource(CachingEnabledObjectState):
     def _compute_value(self):
         # TODO: Implement checking support for non-openness as a prerequisite for heating on certain elements.
 
-        # For now we do not support the heating element position - we just return the object's center position.
-        aabb = self.obj.states['aabb'].get_value()
-        return get_aabb_center(aabb)
+        # Try to get heating element position from URDF
+        try:
+            body_id = self.obj.get_body_id()
+            link_id = link_from_name(body_id, HEATING_ELEMENT_LINK_NAME)
+            heating_element_state = get_link_state(body_id, link_id)
+            return heating_element_state.linkWorldPosition
+        except ValueError:
+            return None
 
     def set_value(self, new_value):
         raise NotImplementedError("Setting heat source capability is not supported.")
