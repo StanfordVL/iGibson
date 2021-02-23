ecca328557c1b2eecaf16288b22500e3b393a31a
Chengshu Li <chengshu@stanford.edu>
2021-02-23 11:40:00 -0800

    make sampling more robust: sample obj slightly higher for uneven supporting surfaces; if checking fails after sampling, sample again; fix UNDER_OBJECT categories

--- gibson2/object_states/on_top.py
+++ gibson2/object_states/on_top.py
@@ -12,12 +12,16 @@ class OnTop(KinematicsMixin, RelativeObjectState, BooleanState):
         return KinematicsMixin.get_dependencies() + ["touching"]
 
     def set_value(self, other, new_value):
-        sampling_success = sample_kinematics(
-            'onTop', self.obj, other, new_value)
-        if sampling_success:
-            clear_cached_states(self.obj)
-            clear_cached_states(other)
-            assert self.get_value(other) == new_value
+        for _ in range(10):
+            sampling_success = sample_kinematics(
+                'onTop', self.obj, other, new_value)
+            if sampling_success:
+                clear_cached_states(self.obj)
+                clear_cached_states(other)
+                if self.get_value(other) != new_value:
+                    sampling_success = False
+            if sampling_success:
+                break
 
         return sampling_success
 
