2383fd5c978d8e84c63f7e17b3ab8ed8f5e92bbf
Cem Gokmen <cgokmen@stanford.edu>
2021-02-25 14:04:33 -0800

    Put demo code under __main__ check.

--- gibson2/examples/demo/cleaning_demo.py
+++ gibson2/examples/demo/cleaning_demo.py
@@ -9,57 +9,63 @@ from gibson2.objects.articulated_object import URDFObject
 from gibson2.utils.assets_utils import get_ig_model_path
 from gibson2.object_states.factory import prepare_object_states
 
-# HDR files for PBR rendering
-hdr_texture = os.path.join(
-    gibson2.ig_dataset_path, 'scenes', 'background', 'probe_02.hdr')
-hdr_texture2 = os.path.join(
-    gibson2.ig_dataset_path, 'scenes', 'background', 'probe_03.hdr')
-light_modulation_map_filename = os.path.join(
-    gibson2.ig_dataset_path, 'scenes', 'Rs_int', 'layout', 'floor_lighttype_0.png')
-background_texture = os.path.join(
-    gibson2.ig_dataset_path, 'scenes', 'background', 'urban_street_01.jpg')
 
-s = Simulator(mode='gui')
+def main():
+    # HDR files for PBR rendering
+    hdr_texture = os.path.join(
+        gibson2.ig_dataset_path, 'scenes', 'background', 'probe_02.hdr')
+    hdr_texture2 = os.path.join(
+        gibson2.ig_dataset_path, 'scenes', 'background', 'probe_03.hdr')
+    light_modulation_map_filename = os.path.join(
+        gibson2.ig_dataset_path, 'scenes', 'Rs_int', 'layout', 'floor_lighttype_0.png')
+    background_texture = os.path.join(
+        gibson2.ig_dataset_path, 'scenes', 'background', 'urban_street_01.jpg')
 
-scene = EmptyScene()
-s.import_scene(scene)
+    s = Simulator(mode='gui')
 
-model_path = os.path.join(get_ig_model_path('sink', 'sink_1'), 'sink_1.urdf')
+    scene = EmptyScene()
+    s.import_scene(scene)
 
-sink = URDFObject(filename=model_path,
-                  category='sink',
-                  name='sink_1',
-                  scale=np.array([0.8, 0.8, 0.8]),
-                  abilities={'toggleable': {}, 'water_source': {}}
-                  )
+    model_path = os.path.join(get_ig_model_path('sink', 'sink_1'), 'sink_1.urdf')
 
-s.import_object(sink)
-sink.set_position([1, 1, 0.8])
-sink.states['toggled_on'].set_value(True)
+    sink = URDFObject(filename=model_path,
+                      category='sink',
+                      name='sink_1',
+                      scale=np.array([0.8, 0.8, 0.8]),
+                      abilities={'toggleable': {}, 'water_source': {}}
+                      )
 
-block = YCBObject(name='036_wood_block')
-s.import_object(block)
-block.set_position([1, 1, 1.8])
-block.abilities = ["soakable", "cleaning_tool"]
-prepare_object_states(block, abilities={"soakable": {}, "cleaning_tool": {}})
-# assume block can soak water
+    s.import_object(sink)
+    sink.set_position([1, 1, 0.8])
+    sink.states['toggled_on'].set_value(True)
 
-model_path = os.path.join(get_ig_model_path('table', '19898'), '19898.urdf')
-desk = URDFObject(filename=model_path,
-                  category='table',
-                  name='19898',
-                  scale=np.array([0.8, 0.8, 0.8]),
-                  abilities={'dustyable': {}}
-                  )
+    block = YCBObject(name='036_wood_block')
+    s.import_object(block)
+    block.set_position([1, 1, 1.8])
+    block.abilities = ["soakable", "cleaning_tool"]
+    prepare_object_states(block, abilities={"soakable": {}, "cleaning_tool": {}})
+    # assume block can soak water
 
-print(desk.states.keys())
-desk.states['dirty'].set_value(True)
-s.import_object(desk)
-desk.set_position([1, -2, 0.4])
+    model_path = os.path.join(get_ig_model_path('table', '19898'), '19898.urdf')
+    desk = URDFObject(filename=model_path,
+                      category='table',
+                      name='19898',
+                      scale=np.array([0.8, 0.8, 0.8]),
+                      abilities={'dustyable': {}}
+                      )
 
-# Main simulation loop
-try:
-    while True:
-        s.step()
-finally:
-    s.disconnect()
+    print(desk.states.keys())
+    desk.states['dirty'].set_value(True)
+    s.import_object(desk)
+    desk.set_position([1, -2, 0.4])
+
+    # Main simulation loop
+    try:
+        while True:
+            s.step()
+    finally:
+        s.disconnect()
+
+
+if __name__ == "__main__":
+    main()
