92dd2a54ab6473c1b18411fd00437bb56703a26f
Cem Gokmen <cgokmen@stanford.edu>
2021-02-26 11:13:27 -0800

    Update soaked logic.

--- gibson2/object_states/soaked.py
+++ gibson2/object_states/soaked.py
@@ -1,3 +1,4 @@
+from gibson2.object_states.contact_bodies import ContactBodies
 from gibson2.object_states.water_source import WaterSource
 from gibson2.object_states.object_state_base import AbsoluteObjectState
 from gibson2.object_states.object_state_base import BooleanState
@@ -19,10 +20,9 @@ class Soaked(AbsoluteObjectState, BooleanState):
         water_source_objs = simulator.scene.get_objects_with_state(WaterSource)
         for water_source_obj in water_source_objs:
             contacted_water_body_ids = set(item[1] for item in list(self.obj.states[ContactBodies].get_value()))
-            for water_source in water_source_obj.states[WaterSource].water_sources:
-                for particle in water_source.particles:
-                    if particle.body_id in contacted_water_body_ids:
-                        self.value = True
+            for particle in water_source_obj.states[WaterSource].water_stream.particles:
+                if particle.body_id in contacted_water_body_ids:
+                    self.value = True
 
     @staticmethod
     def get_optional_dependencies():
