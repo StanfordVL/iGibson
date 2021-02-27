9ad13dd2258d44ecbe045abb9b348747e2d3307f
Jaewoo Jang <jangj8523@capri10.stanford.edu>
2021-02-27 05:00:52 -0800

    add save evaluation results feature in challenge.py

--- gibson2/challenge/challenge.py
+++ gibson2/challenge/challenge.py
@@ -1,9 +1,12 @@
 from gibson2.utils.utils import parse_config
 import numpy as np
+import gibson2
 import json
 import os
 from gibson2.envs.igibson_env import iGibsonEnv
 import logging
+from simple_agent import RandomAgent
+
 logging.getLogger().setLevel(logging.WARNING)
 
 
@@ -12,6 +15,7 @@ class Challenge:
         self.config_file = os.environ['CONFIG_FILE']
         self.split = os.environ['SPLIT']
         self.episode_dir = os.environ['EPISODE_DIR']
+        self.result_output_dir = os.environ['OUTPUT_DIR']
         self.eval_episodes_per_scene = os.environ.get(
             'EVAL_EPISODES_PER_SCENE', 100)
 
@@ -29,7 +33,9 @@ class Challenge:
             assert False, 'unknown task: {}'.format(task)
 
         num_episodes_per_scene = self.eval_episodes_per_scene
+        output_dir = os.path.join(self.result_output_dir, self.split)
         split_dir = os.path.join(self.episode_dir, self.split)
+
         assert os.path.isdir(split_dir)
         num_scenes = len(os.listdir(split_dir))
         assert num_scenes > 0
@@ -74,7 +80,19 @@ class Challenge:
             metrics[key] /= total_num_episodes
             print('Avg {}: {}'.format(key, metrics[key]))
 
+        # save the evaluation results
+        if agent is not None:
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+
+            file_path = os.path.join(output_dir, 'results.json')
+
+            with open(file_path, 'w+') as f:
+                json.dump(metrics, f, sort_keys=True, indent=4)
+
 
 if __name__ == '__main__':
     challenge = Challenge()
     challenge.submit(None)
+    # debugging purposes
+    # challenge.submit(RandomAgent())
