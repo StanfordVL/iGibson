e3ba01a0abb996ee84337fca58bf5fb8162a00ae
Cem Gokmen <cgokmen@stanford.edu>
2021-03-01 15:24:18 -0800

    Add faster implementation of get_object_from_state for iGibson scenes.

--- gibson2/simulator.py
+++ gibson2/simulator.py
@@ -881,9 +881,8 @@ class Simulator:
         """
         # Step the object states in global topological order.
         for state_type in self.object_state_types:
-            for obj in self.scene.get_objects():
-                if state_type in obj.states:
-                    obj.states[state_type].update(self)
+            for obj in self.scene.get_objects_with_state(state_type):
+                obj.states[state_type].update(self)
 
     def step_vr(self, print_stats=False):
         """
