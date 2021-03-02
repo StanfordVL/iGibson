eab118ee60338610e195329d24b413ef825fa7ff
Chengshu Li <chengshu@stanford.edu>
2021-03-02 13:30:12 -0800

    fix bug and simplify for link based state

--- gibson2/object_states/link_based_state_mixin.py
+++ gibson2/object_states/link_based_state_mixin.py
@@ -5,32 +5,32 @@ class LinkBasedStateMixin(object):
     def __init__(self):
         super(LinkBasedStateMixin, self).__init__()
 
-        # This variable indicates that the object does not have the necessary link.
-        self.link_missing = False
-        self.link_id = None
         self.body_id = None
+        self.link_id = None
 
     @staticmethod
     def get_state_link_name():
-        raise ValueError("LinkBasedState child should specify link name by overriding get_state_link_name.")
+        raise ValueError(
+            "LinkBasedState child should specify link name by overriding get_state_link_name.")
 
     def _load_link(self):
-        # If we need the link info, get it now.
-        if self.link_id is None or self.body_id is None:
+        # If we haven't tried to load the link before
+        if self.body_id is None:
             # Get the body id
             self.body_id = self.obj.get_body_id()
 
             try:
-                self.link_id = link_from_name(self.body_id, self.get_state_link_name())
+                self.link_id = link_from_name(
+                    self.body_id, self.get_state_link_name())
             except ValueError:
-                self.link_missing = True
+                self.link_id = None
 
     def get_link_position(self):
-        # Stop if we already tried to find the link & couldn't.
-        if self.link_missing:
-            return
-
-        # Load the link if necessary.
+        # Load the link if it is the first time this function is called
         self._load_link()
+
+        # The necessary link is not found
+        if self.link_id is None:
+            return
 
         return get_link_state(self.body_id, self.link_id).linkWorldPosition
