a40b97df2c76757acb4748e400f0960836b6ab93
Chengshu Li <chengshu@stanford.edu>
2021-03-12 13:18:18 -0800

    cache sampling done for Task 8-9, fix bugs for VerticalAdjacency and sample_orientation

--- gibson2/object_states/vertical_adjacency.py
+++ gibson2/object_states/vertical_adjacency.py
@@ -17,24 +17,24 @@ class VerticalAdjacency(CachingEnabledObjectState):
         above_obj_found = False
         below_obj_found = False
 
-        above_obj_id = [-1]
-        below_obj_id = [-1]
+        above_obj_id = -1
+        below_obj_id = -1
 
         for _ in range(5):
-            ray_result  = p.rayTestBatch(
+            ray_result = p.rayTestBatch(
                 [downwards_ray_start, upwards_ray_start],
                 [downwards_ray_end, upwards_ray_end]
             )
             downwards_ray_start = ray_result[0][3] - np.array([0, 0, 0.001])
             upwards_ray_start = ray_result[1][3] + np.array([0, 0, 0.001])
 
-            if [ray_result[0][0]] != self.obj.body_id and not above_obj_found:
+            if ray_result[0][0] != self.obj.get_body_id() and not above_obj_found:
                 above_obj_found = True
-                above_obj_id = [ray_result[0][0]]
+                above_obj_id = ray_result[0][0]
 
-            if [ray_result[1][0]] != self.obj.body_id and not below_obj_found:
+            if ray_result[1][0] != self.obj.get_body_id() and not below_obj_found:
                 below_obj_found = True
-                below_obj_id = [ray_result[1][0]]
+                below_obj_id = ray_result[1][0]
 
             if above_obj_found and below_obj_found:
                 break
@@ -42,4 +42,5 @@ class VerticalAdjacency(CachingEnabledObjectState):
         return [above_obj_id, below_obj_id]
 
     def set_value(self, new_value):
-        raise NotImplementedError("Adjacency state currently does not support setting.")
+        raise NotImplementedError(
+            "VerticalAdjacency state currently does not support setting.")
