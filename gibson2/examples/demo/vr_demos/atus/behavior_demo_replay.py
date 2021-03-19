924daa0bf2b8adf6325e9b7caf0c3160dfaf6550
Chengshu Li <chengshu@stanford.edu>
2021-03-19 15:10:55 -0700

    fix minor issue of frame range, support both video and folder of images mode

--- gibson2/examples/demo/vr_demos/atus/behavior_demo_replay.py
+++ gibson2/examples/demo/vr_demos/atus/behavior_demo_replay.py
@@ -15,6 +15,7 @@ from gibson2.task.task_base import iGTNTask
 from gibson2.utils.vr_logging import VRLogReader
 import tasknet
 
+
 def parse_args():
     scene_choices = [
         "Beechwood_0_int",
@@ -35,26 +36,35 @@ def parse_args():
     ]
 
     task_choices = [
-	"packing_lunches_filtered",
-	"assembling_gift_baskets_filtered",
-	"organizing_school_stuff_filtered",
-	"re-shelving_library_books_filtered",
-	"serving_hors_d_oeuvres_filtered",
-	"putting_away_toys_filtered",
-	"putting_away_Christmas_decorations_filtered",
-	"putting_dishes_away_after_cleaning_filtered",
-	"cleaning_out_drawers_filtered",
+        "packing_lunches_filtered",
+        "assembling_gift_baskets_filtered",
+        "organizing_school_stuff_filtered",
+        "re-shelving_library_books_filtered",
+        "serving_hors_d_oeuvres_filtered",
+        "putting_away_toys_filtered",
+        "putting_away_Christmas_decorations_filtered",
+        "putting_dishes_away_after_cleaning_filtered",
+        "cleaning_out_drawers_filtered",
     ]
     task_id_choices = [0, 1]
-    parser = argparse.ArgumentParser(description='Run and collect an ATUS demo')
-    parser.add_argument('--task', type=str, required=True, choices=task_choices, nargs='?', help='Name of ATUS task matching PDDL parent folder in tasknet.')
-    parser.add_argument('--task_id', type=int, required=True, choices=task_id_choices, nargs='?', help='PDDL integer ID, matching suffix of pddl.')
-    parser.add_argument('--vr_log_path', type=str, help='Path (and filename) of vr log to replay')
-    parser.add_argument('--frame_save_path', type=str, default='', help='Path to save frames (frame number added automatically, as well as .jpg extension)')
-    parser.add_argument('--scene', type=str, choices=scene_choices, nargs='?', help='Scene name/ID matching iGibson interactive scenes.')
-    parser.add_argument('--disable_scene_cache', action='store_true', help='Whether to disable using pre-initialized scene caches.')
-    parser.add_argument('--profile', action='store_true', help='Whether to print profiling data.')
+    parser = argparse.ArgumentParser(
+        description='Run and collect an ATUS demo')
+    parser.add_argument('--task', type=str, required=True, choices=task_choices,
+                        nargs='?', help='Name of ATUS task matching PDDL parent folder in tasknet.')
+    parser.add_argument('--task_id', type=int, required=True, choices=task_id_choices,
+                        nargs='?', help='PDDL integer ID, matching suffix of pddl.')
+    parser.add_argument('--vr_log_path', type=str,
+                        help='Path (and filename) of vr log to replay')
+    parser.add_argument('--frame_save_path', type=str,
+                        help='Path to save frames (frame number added automatically, as well as .jpg extension)')
+    parser.add_argument('--scene', type=str, choices=scene_choices, nargs='?',
+                        help='Scene name/ID matching iGibson interactive scenes.')
+    parser.add_argument('--disable_scene_cache', action='store_true',
+                        help='Whether to disable using pre-initialized scene caches.')
+    parser.add_argument('--profile', action='store_true',
+                        help='Whether to print profiling data.')
     return parser.parse_args()
+
 
 def main():
     args = parse_args()
@@ -78,7 +88,7 @@ def main():
         env_texture_filename2=hdr_texture2,
         env_texture_filename3=background_texture,
         light_modulation_map_filename=light_modulation_map_filename,
-        enable_shadow=True, 
+        enable_shadow=True,
         enable_pbr=True,
         msaa=False,
         light_dimming_factor=1.0
@@ -87,11 +97,12 @@ def main():
     # Initialize settings to save action replay frames
     vr_replay_settings = VrSettings()
     vr_replay_settings.turn_off_vr_mode()
-    vr_frame_save_path = None if args.frame_save_path == '' else args.frame_save_path
-    vr_replay_settings.set_frame_save_path(vr_frame_save_path)
+    vr_replay_settings.set_frame_save_path(args.frame_save_path)
+    vr_replay_settings.use_companion_window = False
 
     # VR system settings
-    s = Simulator(mode='vr', rendering_settings=vr_rendering_settings, vr_settings=vr_replay_settings)
+    s = Simulator(mode='vr', rendering_settings=vr_rendering_settings,
+                  vr_settings=vr_replay_settings)
     igtn_task = iGTNTask(args.task, args.task_id)
 
     scene_kwargs = None
@@ -99,22 +110,25 @@ def main():
 
     if not args.disable_scene_cache:
         scene_kwargs = {
-                'urdf_file': '{}_task_{}_{}_0_fixed_furniture'.format(args.scene, args.task, args.task_id),
+            'urdf_file': '{}_task_{}_{}_0_fixed_furniture'.format(args.scene, args.task, args.task_id),
         }
         online_sampling = False
 
-    igtn_task.initialize_simulator(simulator=s, scene_id=args.scene, load_clutter=True, scene_kwargs=scene_kwargs, online_sampling=online_sampling)
+    igtn_task.initialize_simulator(simulator=s, scene_id=args.scene, load_clutter=True,
+                                   scene_kwargs=scene_kwargs, online_sampling=online_sampling)
 
     vr_agent = VrAgent(igtn_task.simulator)
 
     if not args.vr_log_path:
         raise RuntimeError('Must provide a VR log path to run action replay!')
-    vr_reader = VRLogReader(args.vr_log_path, s, emulate_save_fps=False, log_status=False)
+    vr_reader = VRLogReader(args.vr_log_path, s,
+                            emulate_save_fps=False, log_status=False)
 
     satisfied_predicates_cached = {}
     while vr_reader.get_data_left_to_read():
         vr_reader.pre_step()
-        igtn_task.simulator.step(print_stats=args.profile, forced_timestep=vr_reader.get_phys_step_n())
+        igtn_task.simulator.step(
+            print_stats=args.profile, forced_timestep=vr_reader.get_phys_step_n())
         task_done, satisfied_predicates = igtn_task.check_success()
 
         # TODO: If this doesn't work, set full_replay to True to guarantee we see the same thing that the VR users saw
@@ -128,6 +142,7 @@ def main():
             satisfied_predicates_cached = satisfied_predicates
 
     s.disconnect()
+
 
 if __name__ == "__main__":
     main()
