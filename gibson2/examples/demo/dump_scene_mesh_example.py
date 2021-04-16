f0bafe5686c21dc573708a6fca7fc5715c3b50c2
Gokul Dharan <gdharan@capri32.stanford.edu>
2021-04-16 02:15:33 -0700

    test script

--- gibson2/examples/demo/dump_scene_mesh_example.py
+++ gibson2/examples/demo/dump_scene_mesh_example.py
@@ -5,34 +5,35 @@ import numpy as np
 from gibson2.render.mesh_renderer.mesh_renderer_cpu import MeshRenderer
 from gibson2.render.profiler import Profiler
 from gibson2.utils.assets_utils import get_ig_scene_path
+from gibson2.simulator import Simulator
+from gibson2.scenes.igibson_indoor_scene import InteractiveIndoorScene
 
 
 def main():
-    global _mouse_ix, _mouse_iy, down, view_direction
 
     if len(sys.argv) > 1:
         model_path = sys.argv[1]
     else:
         model_path = os.path.join(get_ig_scene_path('Rs_int'))
 
-    renderer = MeshRenderer(width=512, height=512)
-    renderer.load_object(model_path)
+    s = Simulator(mode='headless', image_width=512, image_height=512, device_idx=0)
+    scene = InteractiveIndoorScene('Rs_int', texture_randomization=False, object_randomization=False)
+    s.import_ig_scene(scene)
 
-    vert, face = renderer.dump()
+    #vert, face = renderer.dump()
 
-    printf("Vertices Shape: "+ str(vert.shape))
-    printf("Faces Shape: "+ str(face.shape))
+    #printf("Vertices Shape: "+ str(vert.shape))
+    #printf("Faces Shape: "+ str(face.shape))
 
-    renderer.add_instance(0)
-    print(renderer.visual_objects, renderer.instances)
-    print(renderer.materials_mapping, renderer.mesh_materials)
+    #renderer.add_instance(0)
+    #print(renderer.visual_objects, renderer.instances)
+    #print(renderer.materials_mapping, renderer.mesh_materials)
+
+    vert, face = s.renderer.dump()
+    print("Vertices Shape: "+ str(vert.shape))
+    print("Faces Shape: "+ str(face.shape))
 
 
-    printf("Vertices Shape: "+ str(vert.shape))
-    printf("Faces Shape: "+ str(face.shape))
-
-
-    renderer.release()
 
 
 if __name__ == '__main__':
