7f20c084282214aaf1040c860981f4a0f0516955
Cem Gokmen <cgokmen@stanford.edu>
2021-04-25 19:20:39 -0700

    Improved ray casting-based sampling.

--- gibson2/examples/demo/ray_cast_sampling.py
+++ gibson2/examples/demo/ray_cast_sampling.py
@@ -1,5 +1,8 @@
 import os
 
+import numpy as np
+import pdb
+
 import gibson2
 from gibson2 import object_states
 from gibson2.objects.articulated_object import ArticulatedObject
@@ -20,41 +23,54 @@ def main():
 
         cabinet_filename = os.path.join(gibson2.assets_path, 'models/cabinet2/cabinet_0007.urdf')
 
+        microwave_dir = os.path.join(gibson2.ig_dataset_path, 'objects/microwave/7128/')
+        microwave_filename = os.path.join(microwave_dir, '7128.urdf')
+
         plate_dir = os.path.join(gibson2.ig_dataset_path, 'objects/plate/plate_000/')
         plate_filename = os.path.join(plate_dir, 'plate_000.urdf')
 
         apple_dir = os.path.join(gibson2.ig_dataset_path, 'objects/apple/00_0/')
         apple_filename = os.path.join(apple_dir, '00_0.urdf')
 
-        cabinet = ArticulatedObject(filename=cabinet_filename, scale=1.2)
+        cabinet = ArticulatedObject(filename=cabinet_filename, scale=2)
         s.import_object(cabinet)
         cabinet.set_position([0, 0, 0.5])
 
-        for i in range(4):
+        for _ in range(100):
+            s.step()
+
+        microwave = URDFObject(filename=microwave_filename, category="microwave", model_path=microwave_dir,
+                               scale=np.array([0.5, 0.5, 0.5]))
+        s.import_object(microwave)
+        assert microwave.states[object_states.OnTop].set_value(cabinet, True, use_ray_casting_method=True)
+
+        microwave.states[object_states.Open].set_value(True)
+
+        for _ in range(100):
+            s.step()
+
+        print("Microwave placed")
+
+        for i in range(3):
             plate = URDFObject(filename=plate_filename, category="plate", model_path=plate_dir,
-                               bounding_box=[0.25, 0.25, 0.02])
+                               bounding_box=[0.25, 0.25, 0.05])
             s.import_object(plate)
-            plate.set_position([2, 2, 1])
 
-            # Run simulation for 100 steps
+            # Put the 1st plate in the microwave
+            if i == 0:
+                assert plate.states[object_states.Inside].set_value(microwave, True, use_ray_casting_method=True)
+            else:
+                assert plate.states[object_states.OnTop].set_value(microwave, True, use_ray_casting_method=True)
+
+            print("Plate %d placed." % i)
+
             for _ in range(100):
                 s.step()
 
-            plate.states[object_states.OnTop].set_value(cabinet, True, use_ray_casting_method=True)
-
-            for _ in range(100):
-                s.step()
-
-            for j in range(3):
+            for j in range(0):
                 apple = URDFObject(filename=apple_filename, category="apple", model_path=apple_dir)
                 s.import_object(apple)
-                apple.set_position_orientation([3, 3, 1.1], [0, 0, 0, 1])
-
-                # Run simulation for 100 steps
-                for _ in range(100):
-                    s.step()
-
-                apple.states[object_states.OnTop].set_value(plate, True, use_ray_casting_method=True)
+                assert apple.states[object_states.OnTop].set_value(plate, True, use_ray_casting_method=True)
 
                 for _ in range(100):
                     s.step()
@@ -63,6 +79,7 @@ def main():
             s.step()
     finally:
         s.disconnect()
+
 
 if __name__ == "__main__":
     main()
--- gibson2/object_states/inside.py
+++ gibson2/object_states/inside.py
@@ -1,18 +1,54 @@
+import pdb
+
+import numpy as np
+from scipy.spatial.transform.rotation import Rotation
+
+import gibson2
+from gibson2.external.pybullet_tools.utils import get_aabb_center, get_aabb_extent, aabb_contains_point, get_aabb_volume
 from gibson2.object_states import AABB
 from gibson2.object_states.kinematics import KinematicsMixin
 from gibson2.object_states.object_state_base import BooleanState, RelativeObjectState
 from gibson2.object_states.utils import sample_kinematics, clear_cached_states
-from gibson2.external.pybullet_tools.utils import get_aabb_center, get_aabb_extent, aabb_contains_point, get_aabb_volume
-import numpy as np
-import gibson2
-from IPython import embed
+from gibson2.utils import sampling_utils
+
+_RAY_CASTING_PARALLEL_RAY_NORMAL_ANGLE_TOLERANCE = 0.52
+_RAY_CASTING_MAX_ANGLE_WITH_Z_AXIS = 0.17
+_RAY_CASTING_BIMODAL_STDEV_FRACTION = 0.4
+_RAY_CASTING_BIMODAL_MEAN_FRACTION = 0.5
+_RAY_CASTING_MAX_SAMPLING_ATTEMPTS = 100
 
 
 class Inside(KinematicsMixin, RelativeObjectState, BooleanState):
-    def set_value(self, other, new_value):
+    def set_value(self, other, new_value, use_ray_casting_method=False):
         for _ in range(10):
-            sampling_success = sample_kinematics(
-                'inside', self.obj, other, new_value)
+            if use_ray_casting_method:
+                # TODO: Get this to work with non-URDFObject objects.
+                sampling_results = sampling_utils.sample_cuboid_on_object(
+                    other,
+                    num_samples=1,
+                    max_sampling_attempts=_RAY_CASTING_MAX_SAMPLING_ATTEMPTS,
+                    cuboid_dimensions=self.obj.bounding_box,
+                    bimodal_mean_fraction=_RAY_CASTING_BIMODAL_MEAN_FRACTION,
+                    bimodal_stdev_fraction=_RAY_CASTING_BIMODAL_STDEV_FRACTION,
+                    axis_probabilities=[0, 0, 1],
+                    max_angle_with_z_axis=_RAY_CASTING_MAX_ANGLE_WITH_Z_AXIS,
+                    parallel_ray_normal_angle_tolerance=_RAY_CASTING_PARALLEL_RAY_NORMAL_ANGLE_TOLERANCE,
+                    refuse_downwards=True)
+
+                sampled_vector = sampling_results[0][0]
+                sampled_quaternion = sampling_results[0][2]
+
+                sampling_success = sampled_vector is not None
+                if sampling_success:
+                    # Find the delta to the object's AABB centroid
+                    diff = self.obj.scaled_bbxc_in_blf
+
+                    # Rotate it using the quaternion
+                    rotated_diff = Rotation.from_quat(sampled_quaternion).apply(diff)
+                    self.obj.set_position_orientation(sampled_vector - rotated_diff, sampled_quaternion)
+            else:
+                sampling_success = sample_kinematics(
+                    'inside', self.obj, other, new_value)
             if sampling_success:
                 clear_cached_states(self.obj)
                 clear_cached_states(other)
@@ -20,7 +56,7 @@ class Inside(KinematicsMixin, RelativeObjectState, BooleanState):
                     sampling_success = False
                 if gibson2.debug_sampling:
                     print('Inside checking', sampling_success)
-                    embed()
+                    pdb.set_trace()
             if sampling_success:
                 break
 
