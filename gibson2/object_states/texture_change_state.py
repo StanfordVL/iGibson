e56a21620aa2985ccc00b3fbc8d62d8fe9a4b4b6
fxia22 <xf1280@gmail.com>
2021-05-05 21:23:45 -0700

    resolve priority

--- gibson2/object_states/texture_change_state.py
+++ gibson2/object_states/texture_change_state.py
@@ -1,13 +1,23 @@
 from gibson2.object_states.object_state_base import BaseObjectState
-
+from gibson2 import object_states
 
 class TextureChangeState(BaseObjectState):
     def __init__(self, obj):
         super(TextureChangeState, self).__init__(obj)
         self.material = None
 
+        self.resolution_priotiy = {
+            object_states.Burnt: 2,
+            object_states.Cooked: 1,
+            object_states.Soaked: 0,
+        }
+
+        self.current_priority = 0
+
     def update(self, simulator):
         super(TextureChangeState, self).update(simulator)
-
         if self.material is not None:
-            self.material.change_material(self.get_value())
+            if self.get_value():
+                if self.resolution_priotiy[self.__class__] > self.current_priority:
+                    self.material.change_material(self.__class__, self.get_value())
+                    self.current_priority = self.resolution_priotiy[self.__class__]
