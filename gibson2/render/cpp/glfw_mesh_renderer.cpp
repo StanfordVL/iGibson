5d108e5fe32d1c5534abeb2ac6760c96ec6f6119
fxia22 <xf1280@gmail.com>
2021-05-05 22:01:19 -0700

    make procedural material work with OR

--- gibson2/render/cpp/glfw_mesh_renderer.cpp
+++ gibson2/render/cpp/glfw_mesh_renderer.cpp
@@ -157,6 +157,7 @@ PYBIND11_MODULE(GLFWRendererContext, m) {
     // for optimized renderer
     pymodule.def("generateArrayTextures", &GLFWRendererContext::generateArrayTextures, "TBA");
     pymodule.def("renderSetup", &GLFWRendererContext::renderSetup, "TBA");
+    pymodule.def("update_texture_id_arrays", &GLFWRendererContext::update_texture_id_arrays, "TBA");
 	pymodule.def("updateHiddenData", &GLFWRendererContext::updateHiddenData, "TBA");
 	pymodule.def("updateUVData", &GLFWRendererContext::updateUVData, "TBA");
     pymodule.def("updateDynamicData", &GLFWRendererContext::updateDynamicData, "TBA");
