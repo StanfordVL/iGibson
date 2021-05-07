ffb95b979e5e5abc0a87ce1427d8cc628ee8f046
Cem Gokmen <cgokmen@stanford.edu>
2021-05-07 13:39:08 -0700

    Fix circular import.

--- gibson2/scenes/scene_base.py
+++ gibson2/scenes/scene_base.py
@@ -1,8 +1,6 @@
 from abc import ABCMeta, abstractmethod
 
 from future.utils import with_metaclass
-from gibson2.objects.particles import Particle
-
 from gibson2.objects.visual_marker import VisualMarker
 from gibson2.objects.visual_shape import VisualShape
 
@@ -77,6 +75,8 @@ class Scene(with_metaclass(ABCMeta)):
             raise ValueError(
                 "To add an object to an already-loaded scene, use the Simulator's import_object function.")
 
+        # Avoid circular import.
+        from gibson2.objects.particles import Particle
         if isinstance(obj, VisualMarker) or isinstance(obj, VisualShape) or isinstance(obj, Particle):
             raise ValueError(
                 "VisualMarker, VisualShape and Particle objects and subclasses should be added directly to simulator.")
