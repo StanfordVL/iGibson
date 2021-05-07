1c54651f3d4fc5f7cd01c6a04cbbc3a7296d2a59
Cem Gokmen <cgokmen@stanford.edu>
2021-05-06 20:04:35 -0700

    Add fridge to temperature demo.

--- gibson2/examples/demo/temperature_example.py
+++ gibson2/examples/demo/temperature_example.py
@@ -43,17 +43,34 @@ def main():
         oven.set_position([-2, 0, 0.816])
         oven.states[object_states.ToggledOn].set_value(True)
 
+        fridge_dir = os.path.join(
+            gibson2.ig_dataset_path, 'objects/fridge/12252/')
+        fridge_urdf = os.path.join(fridge_dir, "12252.urdf")
+        fridge = URDFObject(fridge_urdf, name="fridge", category="fridge", model_path=fridge_dir, abilities={
+            "cold_source": {
+                "temperature": -100.0,
+                "requires_inside": True,
+            }
+        })
+        s.import_object(fridge)
+        fridge.set_position_orientation([-1, -3, 0.75], [1, 0, 0, 0])
+
         apple_dir = os.path.join(
             gibson2.ig_dataset_path, 'objects/apple/00_0/')
         apple_urdf = os.path.join(apple_dir, "00_0.urdf")
         apple = URDFObject(apple_urdf, name="apple", category="apple", model_path=apple_dir)
         s.import_object(apple)
-        apple.set_position([0, -2, 0.05])
+        apple.set_position([-1, -2, 0.05])
 
         # Run simulation for 1000 steps
         while True:
             s.step()
-            print("Apple Temperature: ", apple.states[object_states.Temperature].get_value())
+            print("Apple Temperature: %.2f. Frozen: %r. Cooked: %r. Burnt: %r." % (
+                apple.states[object_states.Temperature].get_value(),
+                apple.states[object_states.Frozen].get_value(),
+                apple.states[object_states.Cooked].get_value(),
+                apple.states[object_states.Burnt].get_value(),
+            ))
     finally:
         s.disconnect()
 
