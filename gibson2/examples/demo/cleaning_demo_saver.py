71f6ff0194f30344740703ab24a3002542ef9983
Cem Gokmen <cgokmen@stanford.edu>
2021-05-07 01:56:24 -0700

    Add cleaning demo saver & loader.

---
+++ gibson2/examples/demo/cleaning_demo_saver.py
@@ -0,0 +1,35 @@
+from gibson2 import object_states
+from gibson2.object_states.factory import prepare_object_states
+from gibson2.objects.ycb_object import YCBObject
+from gibson2.scenes.igibson_indoor_scene import InteractiveIndoorScene
+from gibson2.simulator import Simulator
+
+
+def main():
+    s = Simulator(mode='gui', image_width=512,
+                  image_height=512, device_idx=0)
+    scene = InteractiveIndoorScene(
+        'Rs_int', texture_randomization=False, object_randomization=False,
+    )
+    s.import_ig_scene(scene)
+
+    # Set everything that can go dirty.
+    stateful_objects = set(
+        scene.get_objects_with_state(object_states.Dusty) + scene.get_objects_with_state(object_states.Stained) +
+        scene.get_objects_with_state(object_states.WaterSource))
+    for obj in stateful_objects:
+        if object_states.Dusty in obj.states:
+            obj.states[object_states.Dusty].set_value(True)
+
+        if object_states.Stained in obj.states:
+            obj.states[object_states.Stained].set_value(True)
+
+        if object_states.WaterSource in obj.states:
+            obj.states[object_states.ToggledOn].set_value(True)
+
+    scene.save_modified_urdf("potato")
+    s.disconnect()
+
+
+if __name__ == "__main__":
+    main()
