5c8896dbd71eb2789f90a7fb33a95e27727e221c
Cem Gokmen <cgokmen@stanford.edu>
2021-05-07 01:56:24 -0700

    Add cleaning demo saver & loader.

---
+++ gibson2/examples/demo/cleaning_demo_loader.py
@@ -0,0 +1,24 @@
+from gibson2 import object_states
+from gibson2.object_states.factory import prepare_object_states
+from gibson2.objects.ycb_object import YCBObject
+from gibson2.scenes.igibson_indoor_scene import InteractiveIndoorScene
+from gibson2.simulator import Simulator
+
+
+def main():
+    s = Simulator(mode='gui', image_width=512,
+                  image_height=512, device_idx=0)
+    scene = InteractiveIndoorScene(
+        'Rs_int', urdf_file="potato",
+        load_object_categories=['breakfast_table'],
+    )
+    s.import_ig_scene(scene)
+    try:
+        while True:
+            s.step()
+    finally:
+        s.disconnect()
+
+
+if __name__ == "__main__":
+    main()
