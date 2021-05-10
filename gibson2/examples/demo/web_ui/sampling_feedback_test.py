0230be49ef0a397059559c24a32b21c4668bfa26
Sanjana Srivastava <sanjana2@stanford.edu>
2021-05-10 16:27:07 -0700

    minor

--- gibson2/examples/demo/web_ui/sampling_feedback_test.py
+++ gibson2/examples/demo/web_ui/sampling_feedback_test.py
@@ -8,19 +8,25 @@ url = TEST_URL
 
 SCENES = [
         "Wainscott_0_int",
-        "Rs_int",
-        "Benevolence_1_int"
+        "Merom_1_int",
+        "Beechwood_0_int"
     ]
-ACTIVITY_NAME = "cleaning_microwave_oven"
+ACTIVITY_NAME = "cleaning_up_after_a_meal"
 OBJECT_LIST = """(:objects
- 	microwave.n.02_1 - microwave.n.02
-	rag.n.01_1 - rag.n.01
-	cabinet.n.01_1 - cabinet.n.01
-	sink.n.01_1 - sink.n.01
+ 	bowl.n.01_1 bowl.n.01_2 - bowl.n.01
+	table.n.02_1 - table.n.02
+	bag.n.01_1 - bag.n.01
+	chair.n.01_1 chair.n.01_2 - chair.n.01
+	plate.n.04_1 plate.n.04_2 plate.n.04_3 plate.n.04_4 - plate.n.04
+	cup.n.01_1 cup.n.01_2 - cup.n.01
+	food.n.02_1 food.n.02_2 - food.n.02
+	floor.n.01_1 floor.n.01_2 - floor.n.01
+	detergent.n.02_1 - detergent.n.02
+	agent.n.01_1 - agent.n.01
 )
 """
-INITIAL_CONDITIONS = "(:init (dusty microwave.n.02_1) (inside rag.n.01_1 cabinet.n.01_1) (inroom microwave.n.02_1 kitchen) (inroom sink.n.01_1 kitchen) (inroom cabinet.n.01_1 kitchen))"
-GOAL_CONDITIONS = "(:goal (and (not (dusty ?microwave.n.02_1)) (inside ?rag.n.01_1 ?sink.n.01_1) (not (inside ?rag.n.01_1 ?cabinet.n.01_1))))"
+INITIAL_CONDITIONS = "(:init (ontop bowl.n.01_1 table.n.02_1) (ontop bowl.n.01_2 table.n.02_1) (stained bowl.n.01_1) (stained bowl.n.01_2) (ontop bag.n.01_1 chair.n.01_1) (ontop plate.n.04_1 table.n.02_1) (ontop plate.n.04_2 table.n.02_1) (ontop plate.n.04_3 table.n.02_1) (ontop plate.n.04_4 table.n.02_1) (stained plate.n.04_1) (stained plate.n.04_2) (stained plate.n.04_3) (stained plate.n.04_4) (ontop cup.n.01_1 table.n.02_1) (ontop cup.n.01_2 table.n.02_1) (stained cup.n.01_1) (stained cup.n.01_2) (ontop food.n.02_1 chair.n.01_2) (onfloor food.n.02_2 floor.n.01_1) (onfloor detergent.n.02_1 floor.n.01_1) (stained chair.n.01_1) (stained chair.n.01_2) (stained table.n.02_1) (inroom floor.n.01_1 dining_room) (inroom floor.n.01_2 kitchen) (inroom table.n.02_1 dining_room) (inroom chair.n.01_1 dining_room) (inroom chair.n.01_2 dining_room) (onfloor agent.n.01_1 floor.n.01_2))"
+GOAL_CONDITIONS = "(:goal (and (and (forall (?bowl.n.01 - bowl.n.01) (not (stained ?bowl.n.01))) (forall (?plate.n.04 - plate.n.04) (not (stained ?plate.n.04))) (forall (?cup.n.01 - cup.n.01) (not (stained ?cup.n.01)))) (forall (?food.n.02 - food.n.02) (inside ?food.n.02 ?bag.n.01_1)) (onfloor ?bag.n.01_1 ?floor.n.01_1) (and (not (stained ?chair.n.01_2)) (not (stained ?table.n.02_1)))))"
 
 
 def run_setup(scenes=None):
