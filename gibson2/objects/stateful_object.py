c9d4dbc64ece99e784155769d1f8b6f2cba38103
Cem Gokmen <cgokmen@stanford.edu>
2021-05-18 18:19:03 -0700

    Make setting object pose reset cached states.

--- gibson2/objects/stateful_object.py
+++ gibson2/objects/stateful_object.py
@@ -1,4 +1,5 @@
 from gibson2.object_states.object_state_base import AbsoluteObjectState
+from gibson2.object_states.utils import clear_cached_states
 from gibson2.objects.object_base import Object
 from gibson2.object_states.factory import prepare_object_states
 
@@ -24,3 +25,19 @@ class StatefulObject(Object):
         for state_type, state_instance in self.states.items():
             if issubclass(state_type, AbsoluteObjectState):
                 state_instance.load(dump[state_type])
+
+    def set_position(self, pos):
+        super(StatefulObject, self).set_position(pos)
+        clear_cached_states(self)
+
+    def set_orientation(self, orn):
+        super(StatefulObject, self).set_orientation(orn)
+        clear_cached_states(self)
+
+    def set_position_orientation(self, pos, orn):
+        super(StatefulObject, self).set_position_orientation(pos, orn)
+        clear_cached_states(self)
+
+    def set_base_link_position_orientation(self, pos, orn):
+        super(StatefulObject, self).set_base_link_position_orientation(pos, orn)
+        clear_cached_states(self)
