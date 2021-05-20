18818744d8bda215ae8cfc310018507a637c1202
fxia22 <xf1280@gmail.com>
2021-05-19 23:52:25 -0700

    use demo from ig_dataset

--- gibson2/test/test_determinism.py
+++ gibson2/test/test_determinism.py
@@ -1,8 +1,8 @@
 import tempfile
-
 import tasknet
-
 from gibson2.examples.demo.vr_demos.atus import behavior_demo_collection, behavior_demo_replay
+import gibson2
+import os
 
 tasknet.set_backend("iGibson")
 
@@ -23,28 +23,13 @@ def test_determinism_with_new_demo():
     # Assert for the completion state.
     assert replay_determinism, "Replay was not deterministic."
 
+def test_determinism_with_existing_vr_demo():
+    DEMO_FILE = os.path.join(gibson2.ig_dataset_path, 'tests',
+                             'clearing_the_table_after_dinner_0_Beechwood_0_int_2021-05-19_23-26-26.hdf5')
+    # Replay the canonical demo.
+    with tempfile.NamedTemporaryFile() as replay_file:
+        replay_determinism = behavior_demo_replay.replay_demo(
+            DEMO_FILE, vr_replay_log_path=replay_file.name, no_vr=True)
 
-# To be enabled once we have a canonical test demo.
-# def test_determinism_with_existing_headless_demo():
-#     DEMO_FILE = ""  # TODO: Put a demo file somewhere, put its path here.
-#
-#     # Replay the random demo.
-#     with tempfile.NamedTemporaryFile() as replay_file:
-#         replay_determinism = behavior_demo_replay.replay_demo(
-#             DEMO_FILE, vr_replay_log_path=replay_file.name, no_vr=True)
-#
-#     # Assert for the completion state.
-#     assert replay_determinism, "Replay was not deterministic."
-
-
-# To be enabled once we have a canonical test demo.
-# def test_determinism_with_existing_vr_demo():
-#     DEMO_FILE = ""  # TODO: Put a demo file somewhere, put its path here.
-#
-#     # Replay the random demo.
-#     with tempfile.NamedTemporaryFile() as replay_file:
-#         replay_determinism = behavior_demo_replay.replay_demo(
-#             DEMO_FILE, vr_replay_log_path=replay_file.name, no_vr=True)
-#
-#     # Assert for the completion state.
-#     assert replay_determinism, "Replay was not deterministic."
+    # Assert for the completion state.
+    assert replay_determinism, "Replay was not deterministic."
