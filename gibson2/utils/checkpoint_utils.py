6aa98012b07a3b119a8eb67653f7792816a17dc8
Chengshu Li <chengshu@stanford.edu>
2021-06-02 22:15:56 -0700

    remove duplicate code for save/load scene from disk and memory, save/load(re-construct) assistive grasping

--- gibson2/utils/checkpoint_utils.py
+++ gibson2/utils/checkpoint_utils.py
@@ -6,25 +6,16 @@ import pybullet as p
 
 
 def save_checkpoint(simulator, root_directory):
-    bullet_path = os.path.join(root_directory, "%d.bullet" % simulator.frame_count)
+    bullet_path = os.path.join(
+        root_directory, "%d.bullet" % simulator.frame_count)
     json_path = os.path.join(root_directory, "%d.json" % simulator.frame_count)
 
     # Save the simulation state.
     p.saveBullet(bullet_path)
 
-    # Dump the object state.
-    object_dump = {}
-    for name, obj in simulator.scene.objects_by_name.items():
-        object_dump[name] = obj.dump_state()
-
-    # Dump the robot state.
-    robot_dump = []
-    for robot in simulator.robots:
-        robot_dump.append(robot.dump_state())
-
     # Save the dump in a file.
     with open(json_path, "w") as f:
-        json.dump({"objects": object_dump, "robots": robot_dump}, f)
+        json.dump(save_internal_states(simulator), f)
 
 
 def load_checkpoint(simulator, root_directory, frame):
@@ -37,12 +28,30 @@ def load_checkpoint(simulator, root_directory, frame):
     with open(json_path, "r") as f:
         dump = json.load(f)
 
-        # Restore the object state.
-        object_dump = dump["objects"]
-        for name, obj in simulator.scene.objects_by_name.items():
-            obj.load_state(object_dump[name])
+    load_internal_states(simulator, dump)
 
-        # Restore the robot state.
-        robot_dumps = dump["robots"]
-        for robot, robot_dump in zip(simulator.robots, robot_dumps):
-            robot.load_state(robot_dump)
+
+def save_internal_states(simulator):
+    # Dump the object state.
+    object_dump = {}
+    for name, obj in simulator.scene.objects_by_name.items():
+        object_dump[name] = obj.dump_state()
+
+    # Dump the robot state.
+    robot_dump = []
+    for robot in simulator.robots:
+        robot_dump.append(robot.dump_state())
+
+    return {"objects": object_dump, "robots": robot_dump}
+
+
+def load_internal_states(simulator, dump):
+    # Restore the object state.
+    object_dump = dump["objects"]
+    for name, obj in simulator.scene.objects_by_name.items():
+        obj.load_state(object_dump[name])
+
+    # Restore the robot state.
+    robot_dumps = dump["robots"]
+    for robot, robot_dump in zip(simulator.robots, robot_dumps):
+        robot.load_state(robot_dump)
