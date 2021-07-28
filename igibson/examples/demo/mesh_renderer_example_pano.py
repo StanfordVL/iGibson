11b000840d42216cf18a30ef8b149d43619c6ce7
fxia22 <xf1280@gmail.com>
2021-07-28 00:25:02 -0700

    add script to compare with gibson1

--- igibson/examples/demo/mesh_renderer_example_pano.py
+++ igibson/examples/demo/mesh_renderer_example_pano.py
@@ -1,8 +1,10 @@
+import json
 import os
 import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image
 
 from igibson.render.mesh_renderer.mesh_renderer_cpu import MeshRenderer
 from igibson.render.mesh_renderer.mesh_renderer_settings import MeshRendererSettings
@@ -15,27 +17,40 @@ def main():
     if len(sys.argv) > 1:
         model_path = sys.argv[1]
     else:
-        model_path = os.path.join(get_scene_path("Rs"), "mesh_z_up.obj")
+        model_path = os.path.join(get_scene_path("Allensville"), "mesh_z_up.obj")
 
     settings = MeshRendererSettings(enable_pbr=False)
-    renderer = MeshRenderer(width=512, height=512, rendering_settings=settings)
+    renderer = MeshRenderer(width=1024, height=1024, rendering_settings=settings)
     renderer.load_object(model_path)
 
     renderer.add_instance(0)
     print(renderer.visual_objects, renderer.instances)
     print(renderer.materials_mapping, renderer.mesh_materials)
 
-    px = 0
-    py = 0.2
+    camera_pose = (
+        np.array(
+            json.load(open("/data2/gibson_tiny/Allensville/pano/points/point_p000001.json"))[-1]["camera_rt_matrix"]
+        )
+        .astype(np.float32)
+        .reshape((4, 4))
+    )
+    print(camera_pose)
 
-    camera_pose = np.array([px, py, 0.5])
-    view_direction = np.array([0, -1, -0.3])
-    renderer.set_camera(camera_pose, camera_pose + view_direction, [0, 0, 1])
-    renderer.set_fov(90)
+    camera_pose = np.linalg.inv(camera_pose)
+    renderer.V = camera_pose
 
     img = renderer.get_equi()
+    img2 = np.array(
+        Image.open("/data2/gibson_tiny/Allensville/pano/rgb/point_p000001_view_equirectangular_domain_rgb.png")
+    )
     print(img.shape)
+    Image.fromarray((img * 255).astype(np.uint8)).save("equi.png")
+    plt.subplot(3, 1, 1)
     plt.imshow(img)
+    plt.subplot(3, 1, 2)
+    plt.imshow(img2)
+    plt.subplot(3, 1, 3)
+    plt.imshow(np.abs(img2[:, :, :3] - img[:, :, :3]))
     plt.show()
 
     renderer.release()
