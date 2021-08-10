bb9d1d228bd6bc53103ddef795b6b4344686f8ca
Chengshu Li <chengshu@stanford.edu>
2021-08-09 22:49:20 -0700

    remove unncessary files and code in activity

--- igibson/activity/tests/batch_sampling_saver.py
+++ igibson/activity/tests/batch_sampling_saver.py
@@ -1,23 +1,21 @@
+import argparse
 import os
 import subprocess
 
 import bddl
 
-selected_tasks = [
-    # 'cleaning_shoes',
-    # 'cleaning_the_hot_tub',
-    # 'cleaning_the_pool',
-    # 'cleaning_toilet',
-    # 'cleaning_up_after_a_meal',
-    # 'waxing_cars_or_other_vehicles',
-]
-
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--max_trials", type=int, default=1, help="Maximum number of trials to try sampling.")
+    parser.add_argument(
+        "--num_initializations", type=int, default=1, help="Number of initialization per PDDL per scene."
+    )
+    parser.add_argument("--start_initialization", type=int, default=0, help="Starting idx for initialization")
+    args = parser.parse_args()
+
     condition_dir = os.path.join(os.path.dirname(bddl.__file__), "activity_conditions")
     for task in sorted(os.listdir(condition_dir)):
-        if task not in selected_tasks:
-            continue
         task_dir = os.path.join(condition_dir, task)
         if os.path.isdir(task_dir):
             for task_id_file in sorted(os.listdir(task_dir)):
@@ -25,8 +23,12 @@ def main():
                 if task_id != "0":
                     continue
                 subprocess.call(
-                    "python sampling_saver.py --task {} --task_id {} --max_trials {} --num_initializations {}".format(
-                        task, task_id, 1, 1
+                    "python sampling_saver.py --task {} --task_id {} --max_trials {} --num_initializations {} --start_initialization {}".format(
+                        task,
+                        task_id,
+                        args.max_trials,
+                        args.num_initializations,
+                        args.start_initialization,
                     ),
                     shell=True,
                 )
