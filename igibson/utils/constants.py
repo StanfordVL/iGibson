7794a44f373d249b56bcef1ba08fed0a0753f42d
Chengshu Li <chengshu@stanford.edu>
2021-08-12 16:35:55 -0700

    hard code non-sampleable object categories

--- igibson/utils/constants.py
+++ igibson/utils/constants.py
@@ -33,9 +33,47 @@ class PyBulletSleepState(IntEnum):
 
 
 # BEHAVIOR-related
-FLOOR_SYNSET = "floor.n.01"
-with open(os.path.join(igibson.ig_dataset_path, "metadata/non_sampleable_categories.txt")) as f:
-    NON_SAMPLEABLE_OBJECTS = [FLOOR_SYNSET] + [line.strip() for line in f.readlines()]
+NON_SAMPLEABLE_OBJECTS = [
+    "bathtub.n.01",
+    "bed.n.01",
+    "bench.n.01",
+    "burner.n.01",
+    "cabinet.n.01",
+    "rug.n.01",
+    "chair.n.01",
+    "chest.n.02",
+    "countertop.n.01",
+    "crib.n.01",
+    "dishwasher.n.01",
+    "door.n.01",
+    "dryer.n.01",
+    "floor.n.01",
+    "floor_lamp.n.01",
+    "electric_refrigerator.n.01",
+    "grandfather_clock.n.01",
+    "heater.n.01",
+    "microwave.n.02",
+    "mirror.n.01",
+    "oven.n.01",
+    "piano.n.01",
+    "photograph.n.01",
+    "rail_fence.n.01",
+    "range_hood.n.01",
+    "shelf.n.01",
+    "shower.n.01",
+    "sink.n.01",
+    "sofa.n.01",
+    "loudspeaker.n.01",
+    "stove.n.01",
+    "table.n.02",
+    "toilet.n.02",
+    "towel_rack.n.01",
+    "treadmill.n.01",
+    "television_receiver.n.01",
+    "wall_clock.n.01",
+    "washer.n.03",
+    "window.n.01",
+]
 MAX_TASK_RELEVANT_OBJS = 50
 TASK_RELEVANT_OBJS_OBS_DIM = 9
 AGENT_POSE_DIM = 6
