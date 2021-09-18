ad305eeb327965ed62161f60769747cc3d001068
Michael Lingelbach <m.j.lbach@gmail.com>
2021-09-17 21:53:29 -0700

    Update

--- transplant_poses.py
+++ transplant_poses.py
@@ -14,43 +14,98 @@ def parse_args():
     parser.add_argument("--manifest", help="manifest of files to resample")
     return parser.parse_args()
 
+def transplant_urdf(old_urdf, new_urdf):
+    old_tree = ET.parse(old_urdf)
+    old_tree_root = old_tree.getroot()
 
-def save_modified_urdf(urdf_path, additional_attribs_by_name={}):
-    """
-    Saves a modified URDF file in the scene urdf directory having all objects added to the scene.
+    new_tree = ET.parse(new_urdf)
+    new_tree_root = new_tree.getroot()
 
-    :param urdf_name: Name of urdf file to save (without .urdf)
-    """
-    scene_tree = ET.parse(os.path.join(igibson.ig_dataset_path, urdf_path))
-    tree_root = scene_tree.getroot()
+    object_update_dict = {}
 
-    for child in tree_root:
-        if child.attrib['name'] == 'fetch_gripper_robot_1':
-            rpy = child.attrib['rpy']
+    for element in new_tree_root:
+        name = element.attrib['name']
+        if name in ['fetch_gripper_robot_1', 'j_fetch_gripper_robot_1']:
+            rpy = None
+            xyz = None
+
+            if element.tag == "link":
+                rpy = element.attrib['rpy']
+                xyz = element.attrib['xyz']
+            elif element.tag == "joint":
+                rpy = element.find('origin').attrib['rpy']
+                xyz = element.find('origin').attrib['xyz']
+
+            assert rpy
+            assert xyz
+
             rpy = list(map(float, rpy.split(' ')))
-            rpy = euler_from_quat(rpy)
-            rpy = " ".join([str(item) for item in rpy])
-            child.attrib['rpy'] = rpy
-        elif child.attrib['name'] == 'j_fetch_gripper_robot_1':
-            rpy = child.find('origin').attrib['rpy']
-            rpy = list(map(float, rpy.split(' ')))
-            rpy = euler_from_quat(rpy)
-            rpy = " ".join([str(item) for item in rpy])
-            child.find('origin').attrib['rpy'] = rpy
+            xyz = list(map(float, xyz.split(' ')))
+            object_update_dict[name] = { 
+              'rpy': rpy,
+              'xyz': xyz,
+            }
 
-    xmlstr = minidom.parseString(ET.tostring(tree_root).replace(b"\n", b"").replace(b"\t", b"")).toprettyxml()
-    with open(os.path.join(igibson.ig_dataset_path, urdf_path), "w") as f:
+    link = ET.SubElement(old_tree_root, "link")
+    link.attrib = {
+        "category": "agent",
+        "name": "fetch_gripper_robot_1",
+        "object_scope": "agent.n.01_1",
+        "rpy": " ".join([str(item) for item in rpy]),
+        "xyz": " ".join([str(item) for item in xyz]),
+    }
+    joint = ET.SubElement(old_tree_root, "joint")
+    joint.attrib = {
+        "name": "j_fetch_gripper_robot_1",
+        "type": "floating",
+    }
+    origin = ET.SubElement(joint, "origin")
+    origin.attrib = {
+        "rpy": " ".join([str(item) for item in rpy]),
+        "xyz": " ".join([str(item) for item in xyz]),
+    }
+    child = ET.SubElement(joint, "child")
+    child.attrib = {
+        "link": "fetch_gripper_robot_1",
+    }
+    parent = ET.SubElement(joint, "parent")
+    parent.attrib = {
+        "link": "world",
+    }
+#     for element in old_tree_root:
+#         name = element.attrib['name'] 
+#         if name in object_update_dict:
+#             rpy = object_update_dict[name]['rpy']
+#             xyz = object_update_dict[name]['xyz']
+#             if element.tag == "link":
+#                 rpy_s = " ".join([str(item) for item in rpy])
+#                 xyz_s = " ".join([str(item) for item in xyz])
+#                 element.attrib['rpy'] = rpy_s
+#                 element.attrib['xyz'] = xyz_s
+#             elif element.tag == "joint":
+#                 rpy_s = " ".join([str(item) for item in rpy])
+#                 xyz_s = " ".join([str(item) for item in xyz])
+#                 element.find('origin').attrib['rpy'] = rpy_s
+#                 element.find('origin').attrib['xyz'] = xyz_s
+
+    xmlstr = minidom.parseString(ET.tostring(old_tree_root).replace(b"\n", b"").replace(b"\t", b"")).toprettyxml()
+    with open(old_urdf, "w") as f:
         f.write(xmlstr)
 
 def main():
     args = parse_args()
     urdf_manfiest = pd.read_csv(args.manifest)
+    urdfs_to_ignore = pd.read_csv("./manifest_requires_resample.csv")
 
     bddl.set_backend("iGibson")
 
     for _, row in urdf_manfiest.iterrows():
         urdf_path = row['urdf_filepath']
-        save_modified_urdf(urdf_path)
+        if row['urdf_path'] in list(urdfs_to_ignore['demos']):
+            continue
+        old_urdf = os.path.join(igibson.ig_dataset_path, urdf_path)
+        new_urdf = os.path.join(igibson.root_path, "data", "ig_dataset_replacement", urdf_path)
+        transplant_urdf(old_urdf, new_urdf)
 
     # df.to_csv("qc_{}.csv".format(args.manifest[-5]))
 
