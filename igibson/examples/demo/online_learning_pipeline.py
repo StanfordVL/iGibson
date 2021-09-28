dd644b597af1d2a3509f0381aa43dbb141911a13
Dhruva Bansal <bdhruva@stanford.edu>
2021-09-28 12:21:46 -0700

    Online learning infra WIP

--- igibson/examples/demo/online_learning_pipeline.py
+++ igibson/examples/demo/online_learning_pipeline.py
@@ -1,17 +1,20 @@
-import igibson
-from igibson.envs.behavior_env import BehaviorEnv
-import torch
 import argparse
-import os
+from igibson.envs.behavior_env import BehaviorEnv
+import igibson
 import numpy as np
-
+import os
+from pynput import keyboard
+import threading as th
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+human_feedback = None
+iterations = 100
+ol_agent = None
 config_file = "behavior_full_observability.yaml"
 env = BehaviorEnv(
     config_file=os.path.join(igibson.example_config_path, config_file),
@@ -38,23 +41,33 @@ class OLNet_taskObs(nn.Module):
         x = self.fc3(x)
         return x
 
-obs = env.reset()
-total_reward = 0
-done = False
-ol_agent = None
+def key_capture_thread():
+    global human_feedback
+    with keyboard.Events() as events:
+        event = events.get(1e6)
+        human_feedback = event
 
-with torch.no_grad():
-    while not(done):
-        task_obs = torch.tensor(obs['task_obs'], dtype=torch.float32).unsqueeze(0).to(device)
-        proprioception = torch.tensor(obs['proprioception'], dtype=torch.float32).unsqueeze(0).to(device)
-        if not ol_agent:
-            ol_agent = OLNet_taskObs(task_obs_dim=task_obs.size()[-1], proprioception_dim=proprioception.size()[-1]).to(device)
-        action = ol_agent(task_obs, proprioception)
-        a = action.cpu().numpy().squeeze(0)
-        a_no_reset = np.concatenate((a[:19], a[20:27])) # we do not allow reset action for agents here
-        obs, reward, done, info = env.step(a_no_reset)
-        total_reward += reward
-        if done:
+def train_ol_model(ol_agent, env, device):
+    global human_feedback
+    with torch.no_grad():
+        th.Thread(target=key_capture_thread, args=(), name='key_capture_thread', daemon=True).start()
+        for _ in range(iterations):
             obs = env.reset()
             total_reward = 0
             done = False
+            while not(done):
+                task_obs = torch.tensor(obs['task_obs'], dtype=torch.float32).unsqueeze(0).to(device)
+                proprioception = torch.tensor(obs['proprioception'], dtype=torch.float32).unsqueeze(0).to(device)
+                if not ol_agent:
+                    ol_agent = OLNet_taskObs(task_obs_dim=task_obs.size()[-1], proprioception_dim=proprioception.size()[-1]).to(device)
+                action = ol_agent(task_obs, proprioception)
+                a = action.cpu().numpy().squeeze(0)
+                a_no_reset = np.concatenate((a[:19], a[20:27])) # we do not allow reset action for agents here
+                obs, reward, done, info = env.step(a_no_reset)
+                if human_feedback:
+                    print(human_feedback)
+                    th.Thread(target=key_capture_thread, args=(), name='key_capture_thread', daemon=True).start()
+                    human_feedback = None
+                total_reward += reward
+
+train_ol_model(ol_agent, env, device)
