a092155e75b66b6432d2135a702f46c36f7c3910
Dhruva Bansal <dhruva.bansal00@gmail.com>
2021-09-29 10:39:16 -0700

    Refactored code to support extension to mouse based input

--- igibson/examples/demo/human_feedback.py
+++ igibson/examples/demo/human_feedback.py
@@ -1,3 +1,5 @@
+import threading as th
+
 from pynput import keyboard
 
 
@@ -9,7 +11,7 @@ from pynput import keyboard
 class HumanFeedback:
     def __init__(self):
         super(HumanFeedback, self).__init__()
-        self.feedback_dictionary = {
+        self.keyboard_feedback_dictionary = {
             keyboard.KeyCode.from_char("w"): [
                 0,
                 1,
@@ -179,3 +181,28 @@ class HumanFeedback:
                 0,
             ],  # -y
         }
+        self.human_keyboard_feedback = None
+        self.run_keyboard_capture_thread()
+
+    def keyboard_capture_thread(self):
+        with keyboard.Events() as events:
+            event = events.get(1e6)
+            self.human_keyboard_feedback = event
+
+    def run_keyboard_capture_thread(self):
+        th.Thread(target=self.keyboard_capture_thread, args=(), name="keyboard_capture_thread", daemon=True).start()
+
+    def return_human_keyboard_feedback(self, action_length):
+        feedback = None
+        if self.human_keyboard_feedback:
+            if "Press" in str(self.human_keyboard_feedback):  # only use keypresses as reward signals
+                if self.human_keyboard_feedback.key in self.keyboard_feedback_dictionary:
+                    feedback = [0 for _ in range(action_length)]
+                    feedback = self.keyboard_feedback_dictionary[self.human_keyboard_feedback.key]
+                elif self.human_keyboard_feedback.key == keyboard.KeyCode.from_char("p"):  # use 'p' for pausing:
+                    feedback = "Pause"
+
+            self.run_keyboard_capture_thread()
+
+        self.human_keyboard_feedback = None
+        return feedback
--- igibson/examples/demo/online_learning_pipeline.py
+++ igibson/examples/demo/online_learning_pipeline.py
@@ -13,8 +13,6 @@ from pynput import keyboard
 import igibson
 from igibson.envs.behavior_env import BehaviorEnv
 
-human_feedback = None
-
 
 class OLNet_taskObs(nn.Module):
     def __init__(self, task_obs_dim=456, proprioception_dim=20, num_actions=26):
@@ -34,24 +32,14 @@ class OLNet_taskObs(nn.Module):
         return x
 
 
-def key_capture_thread():
-    global human_feedback
-    with keyboard.Events() as events:
-        event = events.get(1e6)
-        human_feedback = event
-
-
 def train_ol_model(ol_agent, env, device, learning_rate):
-    global human_feedback
     optimizer = None
-    feedback_dictionary = HumanFeedback().feedback_dictionary
-    th.Thread(target=key_capture_thread, args=(), name="key_capture_thread", daemon=True).start()
+    human_feedback = HumanFeedback()
     for _ in range(iterations):
         obs = env.reset()
         total_reward = 0
         done = False
         paused = False
-
         while not done:
             task_obs = torch.tensor(obs["task_obs"], dtype=torch.float32).unsqueeze(0).to(device)
             proprioception = torch.tensor(obs["proprioception"], dtype=torch.float32).unsqueeze(0).to(device)
@@ -61,6 +49,7 @@ def train_ol_model(ol_agent, env, device, learning_rate):
                 ).to(device)
                 ol_agent.train()
                 optimizer = optim.Adam(ol_agent.parameters())
+
             optimizer.zero_grad()
             action = ol_agent(task_obs, proprioception)
             a = action.cpu().detach().numpy().squeeze(0)
@@ -68,27 +57,18 @@ def train_ol_model(ol_agent, env, device, learning_rate):
                 obs, reward, done, _ = env.step(a)
             else:
                 reward = 0
+            curr_keyboard_feedback = human_feedback.return_human_keyboard_feedback(action.size()[-1])
+            if curr_keyboard_feedback:
+                if "Pause" in curr_keyboard_feedback:
+                    paused = not paused
+                else:
+                    error = np.array(curr_keyboard_feedback) * learning_rate
+                    label_action = torch.from_numpy(a + error).type(torch.FloatTensor).view(action.size()).to(device)
+                    loss = nn.MSELoss()(action, label_action)
+                    loss.backward()
+                    optimizer.step()
+                    print(loss)
 
-            if human_feedback:
-                if "Press" in str(human_feedback):  # only use keypresses as reward signals
-                    if human_feedback.key in feedback_dictionary:
-                        feedback = [0 for _ in range(action.size()[-1])]
-                        feedback = feedback_dictionary[human_feedback.key]
-                        error = np.array(feedback) * learning_rate
-                        label_action = (
-                            torch.from_numpy(a + error).type(torch.FloatTensor).view(action.size()).to(device)
-                        )
-                        loss = 100 * nn.MSELoss()(action, label_action)
-                        loss.backward()
-                        optimizer.step()
-                        print(loss)
-                    elif human_feedback.key == keyboard.KeyCode.from_char("p"):  # use 'p' for pausing
-                        paused = not paused
-                    else:
-                        print("Invalid feedback received")
-
-                th.Thread(target=key_capture_thread, args=(), name="key_capture_thread", daemon=True).start()
-                human_feedback = None
             total_reward += reward
 
 
@@ -100,7 +80,7 @@ if __name__ == "__main__":
     config_file = "behavior_full_observability.yaml"
     env = BehaviorEnv(
         config_file=os.path.join(igibson.example_config_path, config_file),
-        mode="headless",
+        mode="gui",
         action_timestep=1 / 30.0,
         physics_timestep=1 / 300.0,
         action_filter="all",
