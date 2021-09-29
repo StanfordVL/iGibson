fbf3f1d889e9972680f531982bac2cee034fa65a
Dhruva Bansal <dhruva.bansal00@gmail.com>
2021-09-28 18:19:15 -0700

    Code reformatting

--- igibson/examples/demo/bc_agent.py
+++ igibson/examples/demo/bc_agent.py
@@ -3,15 +3,17 @@ import os
 
 import numpy as np
 import torch
+from simple_bc_agent import BCNet_rgbp, BCNet_taskObs
 
 import igibson
 from igibson.envs.behavior_env import BehaviorEnv
-from simple_bc_agent import BCNet_taskObs, BCNet_rgbp
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Evaluate the trained behvaior_cloning agent in behavior")
     parser.add_argument("--model", type=str, help="Path and filename of trained model")
     return parser.parse_args()
+
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 args = parse_args()
@@ -24,7 +26,7 @@ env = BehaviorEnv(
     mode="gui",
     action_timestep=1 / 30.0,
     physics_timestep=1 / 300.0,
-    action_filter = "all"
+    action_filter="all",
 )
 
 obs = env.reset()
@@ -32,12 +34,12 @@ total_reward = 0
 done = False
 
 with torch.no_grad():
-    while not(done):
-        task_obs = torch.tensor(obs['task_obs'], dtype=torch.float32).unsqueeze(0).to(device)
-        proprioception = torch.tensor(obs['proprioception'], dtype=torch.float32).unsqueeze(0).to(device)
+    while not (done):
+        task_obs = torch.tensor(obs["task_obs"], dtype=torch.float32).unsqueeze(0).to(device)
+        proprioception = torch.tensor(obs["proprioception"], dtype=torch.float32).unsqueeze(0).to(device)
         action = bc_agent(task_obs, proprioception)
         a = action.cpu().numpy().squeeze(0)
-        a_no_reset = np.concatenate((a[:19], a[20:27])) # we do not allow reset action for agents here
+        a_no_reset = np.concatenate((a[:19], a[20:27]))  # we do not allow reset action for agents here
         obs, reward, done, info = env.step(a_no_reset)
         total_reward += reward
         print(total_reward, info)
--- igibson/examples/demo/online_learning_pipeline.py
+++ igibson/examples/demo/online_learning_pipeline.py
@@ -1,38 +1,28 @@
 import argparse
 import os
+import threading as th
 
 import numpy as np
-from pynput import keyboard
-import threading as th
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from pynput import keyboard
 
 import igibson
 from igibson.envs.behavior_env import BehaviorEnv
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
 human_feedback = None
-iterations = 100
-ol_agent = None
-config_file = "behavior_full_observability.yaml"
-env = BehaviorEnv(
-    config_file=os.path.join(igibson.example_config_path, config_file),
-    mode="gui",
-    action_timestep=1 / 30.0,
-    physics_timestep=1 / 300.0,
-    action_filter = "all"
-)
+
 
 class OLNet_taskObs(nn.Module):
-    def __init__(self, task_obs_dim = 456, proprioception_dim=20, num_actions=28):
-        super(OLNet_taskObs, self).__init__()        
+    def __init__(self, task_obs_dim=456, proprioception_dim=20, num_actions=28):
+        super(OLNet_taskObs, self).__init__()
         # image feature
         self.fc1 = nn.Linear(task_obs_dim + proprioception_dim, 1024)
         self.fc2 = nn.Linear(1024, 256)
         self.fc3 = nn.Linear(256, num_actions)
-        
+
     def forward(self, task_obs, proprioceptions):
         x = torch.cat((task_obs, proprioceptions), dim=1)
         x = self.fc1(x)
@@ -42,39 +32,58 @@ class OLNet_taskObs(nn.Module):
         x = self.fc3(x)
         return x
 
+
 def key_capture_thread():
     global human_feedback
     with keyboard.Events() as events:
         event = events.get(1e6)
         human_feedback = event
 
+
 def train_ol_model(ol_agent, env, device):
     global human_feedback
     with torch.no_grad():
-        th.Thread(target=key_capture_thread, args=(), name='key_capture_thread', daemon=True).start()
+        th.Thread(target=key_capture_thread, args=(), name="key_capture_thread", daemon=True).start()
         for _ in range(iterations):
             obs = env.reset()
             total_reward = 0
             done = False
-            while not(done):
-                task_obs = torch.tensor(obs['task_obs'], dtype=torch.float32).unsqueeze(0).to(device)
-                proprioception = torch.tensor(obs['proprioception'], dtype=torch.float32).unsqueeze(0).to(device)
+            while not done:
+                task_obs = torch.tensor(obs["task_obs"], dtype=torch.float32).unsqueeze(0).to(device)
+                proprioception = torch.tensor(obs["proprioception"], dtype=torch.float32).unsqueeze(0).to(device)
                 if not ol_agent:
-                    ol_agent = OLNet_taskObs(task_obs_dim=task_obs.size()[-1], proprioception_dim=proprioception.size()[-1]).to(device)
+                    ol_agent = OLNet_taskObs(
+                        task_obs_dim=task_obs.size()[-1], proprioception_dim=proprioception.size()[-1]
+                    ).to(device)
                 action = ol_agent(task_obs, proprioception)
                 a = action.cpu().numpy().squeeze(0)
                 a_no_reset = np.concatenate((a[:19], a[20:27]))
                 obs, reward, done, info = env.step(a_no_reset)
                 if human_feedback:
-                    if 'Press' in str(human_feedback): # only use keypresses as reward signals
-                        if human_feedback.key == keyboard.KeyCode.from_char('s'):
+                    if "Press" in str(human_feedback):  # only use keypresses as reward signals
+                        if human_feedback.key == keyboard.KeyCode.from_char("s"):
                             print("Negative feeddback received")
-                        elif human_feedback.key == keyboard.KeyCode.from_char('d'):
+                        elif human_feedback.key == keyboard.KeyCode.from_char("d"):
                             print("Positive feedback received")
                         else:
                             print("Invalid feedback received")
-                    th.Thread(target=key_capture_thread, args=(), name='key_capture_thread', daemon=True).start()
+                    th.Thread(target=key_capture_thread, args=(), name="key_capture_thread", daemon=True).start()
                     human_feedback = None
                 total_reward += reward
 
-train_ol_model(ol_agent, env, device)
+
+if __name__ == "__main__":
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    iterations = 100
+    ol_agent = None
+    config_file = "behavior_full_observability.yaml"
+    env = BehaviorEnv(
+        config_file=os.path.join(igibson.example_config_path, config_file),
+        mode="gui",
+        action_timestep=1 / 30.0,
+        physics_timestep=1 / 300.0,
+        action_filter="all",
+    )
+
+    train_ol_model(ol_agent, env, device)
