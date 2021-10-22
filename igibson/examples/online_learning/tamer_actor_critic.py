a867db92f83f42cf3491ae3318fe6a0ae38698d2
Dhruva Bansal <dhruva.bansal00@gmail.com>
2021-10-21 21:06:01 -0700

    TAMER config files and model checkpoints

--- igibson/examples/online_learning/tamer_actor_critic.py
+++ igibson/examples/online_learning/tamer_actor_critic.py
@@ -5,12 +5,14 @@ import sys
 import time
 from copy import deepcopy
 from logging import Handler
+from igibson.utils.checkpoint_utils import load_checkpoint
 
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import yaml
 from human_feedback import HumanFeedback
 from online_learning_interface import FeedbackInterface
 from PyQt5.QtWidgets import *
@@ -74,6 +76,7 @@ def sac(
     num_test_episodes=10,
     max_ep_len=1000,
     save_freq=1,
+    load_model=None
 ):
     """
     Soft Actor-Critic (SAC)
@@ -144,6 +147,9 @@ def sac(
         logger_kwargs (dict): Keyword args for EpochLogger.
         save_freq (int): How often (in terms of gap between epochs) to save
             the current policy and value function.
+        load_model (int): Epoch corresponding to the model checkpoint that needs
+            to be loaded. None if training needs to be done from scratch. Model
+            name format must be tamer_sac_epoch_{load_model}.pt. 
     """
     torch.manual_seed(seed)
     np.random.seed(seed)
@@ -161,6 +167,8 @@ def sac(
 
     # Create actor-critic module and target networks
     ac = actor_critic(obs_dim, env.action_space, **ac_kwargs)
+    if load_model:
+        ac.load_state_dict(torch.load(f"tamer_sac_epoch_{load_model}.pt"))
     ac_targ = deepcopy(ac)
 
     # Freeze target networks with respect to optimizers (only update via polyak averaging)
@@ -331,21 +339,16 @@ def sac(
         if (t + 1) % steps_per_epoch == 0:
             epoch = (t + 1) // steps_per_epoch
             # Test the performance of the deterministic version of the agent.
+            if epoch % save_freq == 0:
+                torch.save(ac.state_dict(), f"tamer_sac_epoch_{epoch}.pt")
             test_agent()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--env", type=str, default="HalfCheetah-v2")
-    parser.add_argument("--hid", type=int, default=256)
-    parser.add_argument("--l", type=int, default=2)
-    parser.add_argument("--gamma", type=float, default=0.99)
-    parser.add_argument("--seed", "-s", type=int, default=42)
-    parser.add_argument("--epochs", type=int, default=50)
-    parser.add_argument("--exp_name", type=str, default="sac")
-    args = parser.parse_args()
+    with open("configs/tamer_sac.yaml", "r") as f:
+        config_data = yaml.load(f, Loader=yaml.FullLoader)
 
-    config_file = "behavior_full_observability.yaml"
+    config_file = config_data['env_config_file']
     env = BehaviorEnv(
         config_file=os.path.join("../configs/", config_file),
         mode="gui",
@@ -355,12 +358,14 @@ if __name__ == "__main__":
     )
 
     torch.set_num_threads(torch.get_num_threads())
-
+    epoch_to_load = config_data["load_model"] if config_data["load_model"] != "None" else None
     sac(
         lambda: env,
         actor_critic=MLPActorCritic,
-        ac_kwargs=dict(hidden_sizes=[args.hid] * args.l),
-        gamma=args.gamma,
-        seed=args.seed,
-        epochs=args.epochs,
+        ac_kwargs=dict(hidden_sizes=[config_data["hidden_dim"]] * config_data["num_layers"]),
+        gamma=config_data["gamma"],
+        seed=config_data["seed"],
+        epochs=config_data["epochs"],
+        load_model=epoch_to_load
+
     )
