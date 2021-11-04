ae675c90de02a2c507140daa96e9de6133018aa8
Gokul Dharan <gdharan@capri10.stanford.edu>
2021-11-03 22:50:31 -0700

    Reverb gain tuning

--- igibson/audio/reverb_visualization.py
+++ igibson/audio/reverb_visualization.py
@@ -23,48 +23,88 @@ import time
 from audio_system import AudioSystem, AcousticMesh
 from math import cos, sin, atan2, sqrt, pi, factorial
 
-def mp3d_example():
+src_f = "10s_speech_sample.wav"
+
+def plotPairedSpectrograms(nameAndData, title):
+    num_plots = len(nameAndData)
+    fig, axs = plt.subplots(1, num_plots, figsize=[6.4,2.1])
+    if title:
+        fig.suptitle(title)
+    for i, ax in enumerate(axs):
+        ax.specgram(nameAndData[i][1], Fs=44100, scale='dB', NFFT=int(0.025*44100), noverlap=int(0.015*44100))
+        ax.title.set_text(nameAndData[i][0])
+        ax.set_xlim([0,2])
+        ax.set_yscale('log')
+        ax.set_ylim([20,1*10**4])
+        #ax.set_ylim([0,10000])
+    plt.savefig(title)
+    
+def plotSpectrogram(data, name):
+    plt.specgram(data, Fs=44100, scale='dB')
+    plt.xlim([0,10])
+    plt.ylim([0,20000])
+    plt.yscale('log')
+    plt.savefig(name)
+    plt.close()
+
+def renderAndPlot(scene_id, pos, name, reverb_gain, mat_override=None):
     s = Simulator(mode='iggui', image_width=512, image_height=512, device_idx=0)
-    scene = StaticIndoorScene('1pXnuDYAj8r')
+    scene = StaticIndoorScene(scene_id)
     #scene = StadiumScene()
     s.import_scene(scene)
 
-
-
-    acousticMesh = getMatterportAcousticMesh(s, "/cvgl/group/Gibson/matterport3d-downsized/v2/1pXnuDYAj8r/sem_map.png")#AcousticMesh()
-    #acousticMesh = AcousticMesh()
-    #transparent_id =ResonanceMaterialToId["Metal"]
-    #Make mesh transparent so we only render direct sound
-    #acousticMesh.materials = np.ones(acousticMesh.materials.shape) * transparent_id
+    acousticMesh = getMatterportAcousticMesh(s, "/cvgl/group/Gibson/matterport3d-downsized/v2/{}/sem_map.png".format(scene_id))#AcousticMesh()
+    if mat_override is not None:
+        acousticMesh.materials = np.ones(acousticMesh.materials.shape) * mat_override
 
     # 10.7, -1.9, 3.5 (small: 6.3, 6.9)
-    obj = cube.Cube(pos=[10.7, -1.9, 2], dim=[0.05, 0.05, 0.05], visual_only=True, mass=0, color=[1,0,0,1])
+    pos3d = pos + [1.2]
+    obj = cube.Cube(pos=pos3d, dim=[0.05, 0.05, 0.05], visual_only=True, mass=0, color=[1,0,0,1])
     obj_id = s.import_object(obj)[0]
 
-    s.viewer = Viewer(simulator=s, renderer=s.renderer, initial_pos=[10.7, -1.9, 1.2])
+    pos3d = pos + [1.2]
+    s.viewer = Viewer(simulator=s, renderer=s.renderer, initial_pos=pos3d)
     s.viewer.renderer = s.renderer
 
     # Audio System Initialization, with reverb/reflections off
-    audioSystem = AudioSystem(s, s.viewer, acousticMesh, is_Viewer=True, writeToFile=True, SR = 44100, num_probes=5, renderAmbisonics=True, renderReverbReflections=True)
+    audioSystem = AudioSystem(s, s.viewer, acousticMesh, is_Viewer=True, writeToFile=name, SR = 44100, num_probes=5, renderAmbisonics=True, renderReverbReflections=True)
     # Attach wav file to imported cube obj
-    audioSystem.registerSource(obj_id, "gettysburg.wav", enabled=True)
+    
+    audioSystem.registerSource(obj_id, src_f, enabled=True, reverb_gain=reverb_gain)
     # Ensure source continuously repeats
-    audioSystem.setSourceRepeat(obj_id)
+    audioSystem.setSourceRepeat(obj_id, False)
 
     s.attachAudioSystem(audioSystem)
-
-    state = True
-    for i in range(70):
-        if i % 68 == 0:
-            audioSystem.setSourceEnabled(obj_id, state)
-            state = not state
+    for i in range(int(10.5 / 0.03)):
         s.step()
     rendered_audio = audioSystem.complete_output[::2]
-    powerSpectrum, frequenciesFound, time, imageAxis = plt.specgram(rendered_audio, Fs=44100)
-    plt.savefig('audio_spectrogram')
+    plotSpectrogram(rendered_audio, name)
     audioSystem.disconnect()
     s.disconnect()
-    
+
+    return rendered_audio
+
+def mp3d_example():
+    src_audio = np.frombuffer(wave.open(src_f, 'rb').readframes(np.inf), dtype=np.int16)
+    plotSpectrogram(src_audio, 'src_spectrogram')
+
+    s_g2_metal = renderAndPlot('1pXnuDYAj8r', [ 1.7, -9.5 ], 'small_gain2_metal', 2, mat_override=ResonanceMaterialToId['Metal'])
+    l_g2_metal = renderAndPlot('1pXnuDYAj8r', [ 10.7, -1.9], 'large_gain2_metal', 2, mat_override=ResonanceMaterialToId['Metal'])
+    plotPairedSpectrograms([("Source", src_audio), ("Small Room", s_g2_metal), ("Large Room", l_g2_metal)] , "Gain 2 (Metal Mesh)")
+    s = renderAndPlot('1pXnuDYAj8r', [ 1.7, -9.5 ], 'small_gain1', 1, mat_override=None)
+    l = renderAndPlot('1pXnuDYAj8r', [ 10.7, -1.9], 'large_gain1', 1, mat_override=None)
+    plotPairedSpectrograms([("Source", src_audio), ("Small Room", s), ("Large Room", l)] , "Gain 1")
+    s = renderAndPlot('1pXnuDYAj8r', [ 1.7, -9.5 ], 'small_gain2', 2, mat_override=None)
+    l = renderAndPlot('1pXnuDYAj8r', [ 10.7, -1.9], 'large_gain2', 2, mat_override=None)
+    plotPairedSpectrograms([("Source", src_audio), ("Small Room", s), ("Large Room", l)] , "Gain 2")
+    s = renderAndPlot('1pXnuDYAj8r', [ 1.7, -9.5 ], 'small_gain4', 4, mat_override=None)
+    l = renderAndPlot('1pXnuDYAj8r', [ 10.7, -1.9], 'large_gain4', 4, mat_override=None)
+    plotPairedSpectrograms([("Source", src_audio), ("Small Room", s), ("Large Room", l)] , "Gain 4")
+    s = renderAndPlot('1pXnuDYAj8r', [ 1.7, -9.5 ], 'small_gain6', 6, mat_override=None)
+    l = renderAndPlot('1pXnuDYAj8r', [ 10.7, -1.9], 'large_gain6', 6, mat_override=None)
+    plotPairedSpectrograms([("Source", src_audio), ("Small Room", s), ("Large Room", l)] , "Gain 6")
+
+
 def main():
     mp3d_example()
     #ig_example()
