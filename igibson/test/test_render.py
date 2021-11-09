0d04a62c1f458c3a723331f3ff19b07a827232b7
Chengshu Li <chengshu@stanford.edu>
2021-11-09 15:50:48 -0800

    ci: add back render tensor test (#562)

--- igibson/test/test_render.py
+++ igibson/test/test_render.py
@@ -112,31 +112,3 @@ def test_render_rendering_cleaning():
         GPUtil.showUtilization()
         renderer.release()
         GPUtil.showUtilization()
-
-
-"""
-def test_tensor_render_rendering():
-    w = 800
-    h = 600
-    renderer = MeshTensorRenderer(w, h)
-    print('before load')
-    renderer.load_object(os.path.join(dir, 'mesh/bed1a77d92d64f5cbbaaae4feed64ec1_new.obj'))
-    print('after load')
-    renderer.set_camera([0, 0, 1.2], [0, 1, 1.2], [0, 1, 0])
-    renderer.set_fov(90)
-    tensor = torch.cuda.ByteTensor(h, w, 4)
-    tensor2 = torch.cuda.ByteTensor(h, w, 4)
-    renderer.render(tensor, tensor2)
-
-    img_np = tensor.flip(0).data.cpu().numpy().reshape(h, w, 4)
-    img_np2 = tensor2.flip(0).data.cpu().numpy().reshape(h, w, 4)
-    # plt.imshow(np.concatenate([img_np, img_np2], axis=1))
-    # plt.show()
-    assert (np.allclose(np.mean(img_np.astype(np.float32), axis=(0, 1)),
-                        np.array([131.71548, 128.34981, 121.81708, 255.86292]), rtol=1e-3))
-    assert (np.allclose(np.mean(img_np2.astype(np.float32), axis=(0, 1)), np.array([154.2405, 0., 0., 255.86292]),
-                        rtol=1e-3))
-    # print(np.mean(img_np.astype(np.float32), axis = (0,1)))
-    # print(np.mean(img_np2.astype(np.float32), axis = (0,1)))
-    renderer.release()
-"""
