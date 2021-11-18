d9c7396b4552f17be6a4ca65d64f92cd0aa519ff
Chengshu Li <chengshu@stanford.edu>
2021-11-18 13:40:01 -0800

    address comments

--- igibson/examples/behavior/behavior_demo_checkpoints.py
+++ igibson/examples/behavior/behavior_demo_checkpoints.py
@@ -4,7 +4,7 @@ import os
 import bddl
 
 import igibson
-from igibson.examples.demo.vr_demos.atus.behavior_demo_replay import safe_replay_demo
+from igibson.examples.behavior.behavior_demo_replay import safe_replay_demo
 from igibson.utils.checkpoint_utils import save_checkpoint
 
 
@@ -14,15 +14,15 @@ def create_checkpoints(demo_file, checkpoint_directory, checkpoint_every_n_steps
         if not env.task.current_success and env.simulator.frame_count % checkpoint_every_n_steps == 0:
             save_checkpoint(env.simulator, checkpoint_directory)
 
-    safe_replay_demo(demo_file, mode="headless", step_callback=step_callback)
+    safe_replay_demo(demo_file, mode="headless", step_callbacks=[step_callback])
 
 
 def main():
-    demo_file = os.path.join(igibson.ig_dataset_path, "tests", "storing_food_0_Rs_int_2021-05-31_11-49-30.hdf5")
+    demo_file = os.path.join(igibson.ig_dataset_path, "tests", "cleaning_windows_0_Rs_int_2021-05-23_23-11-46.hdf5")
     checkpoint_directory = "checkpoints"
     if not os.path.exists(checkpoint_directory):
         os.mkdir(checkpoint_directory)
-    create_checkpoints(demo_file, checkpoint_directory, 50)
+    create_checkpoints(demo_file, checkpoint_directory, 30)
 
 
 if __name__ == "__main__":
