c0e6c4b1227353e27c6a30b31f9dd70ea269097c
corgiTrax <zharu@stanford.edu>
2021-11-17 21:02:43 -0800

    fix camera not rotating / flipping issues by calculating the up_direction relative to the current pose

--- igibson/render/mesh_renderer/mesh_renderer_cpu.py
+++ igibson/render/mesh_renderer/mesh_renderer_cpu.py
@@ -1360,7 +1360,8 @@ class MeshRenderer(object):
                 orn = instance.robot.eyes.get_orientation()
                 mat = quat2rotmat(xyzw2wxyz(orn))[:3, :3]
                 view_direction = mat.dot(np.array([1, 0, 0]))
-                self.set_camera(camera_pos, camera_pos + view_direction, [0, 0, 1], cache=True)
+                up_direction = mat.dot(np.array([0, 0, 1]))
+                self.set_camera(camera_pos, camera_pos + view_direction, up_direction, cache=True)
                 hidden_instances = []
                 if self.rendering_settings.hide_robot:
                     hidden_instances.append(instance)
@@ -1936,7 +1937,8 @@ class MeshRenderer(object):
                 orn = instance.robot.eyes.get_orientation()
                 mat = quat2rotmat(xyzw2wxyz(orn))[:3, :3]
                 view_direction = mat.dot(np.array([1, 0, 0]))
-                self.set_camera(camera_pos, camera_pos + view_direction, [0, 0, 1])
+                up_direction = mat.dot(np.array([0, 0, 1]))
+                self.set_camera(camera_pos, camera_pos + view_direction, up_direction)
 
         original_fov = self.vertical_fov
         self.set_fov(90)
@@ -1989,7 +1991,8 @@ class MeshRenderer(object):
                     orn = instance.robot.eyes.get_orientation()
                     mat = quat2rotmat(xyzw2wxyz(orn))[:3, :3]
                     view_direction = mat.dot(np.array([1, 0, 0]))
-                    self.set_camera(camera_pos, camera_pos + view_direction, [0, 0, 1])
+                    up_direction = mat.dot(np.array([0, 0, 1]))
+                    self.set_camera(camera_pos, camera_pos + view_direction, up_direction)
 
         def render_cube():
             frames = []
