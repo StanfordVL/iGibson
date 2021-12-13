96495688646da75588cb650d7460e6867d96c3b9
Cem Gökmen <cem@cemgokmen.com>
2021-12-13 11:50:46 -0800

    Fix lint-failing file.

--- igibson/render/mesh_renderer/mesh_renderer_tensor.py
+++ igibson/render/mesh_renderer/mesh_renderer_tensor.py
@@ -110,7 +110,6 @@ try:
             )
             return self.readbuffer_to_tensor(modes)
 
-
 except ImportError:
     logging.warning("torch is not available, falling back to rendering to memory (instead of tensor)")
     MeshRendererG2G = MeshRenderer
