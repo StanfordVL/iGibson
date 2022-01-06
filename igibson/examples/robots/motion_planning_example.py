78f857b14f1884650f7d7152ac3569870e5a2d70
josiah_wong <84cremebrule@gmail.com>
2022-01-06 12:07:44 -0800

    clean up MP example

--- igibson/examples/robots/motion_planning_example.py
+++ igibson/examples/robots/motion_planning_example.py
@@ -6,7 +6,46 @@ import numpy as np
 
 import igibson
 from igibson.envs.igibson_env import iGibsonEnv
+from igibson.utils.constants import ViewerMode
 from igibson.utils.motion_planning_wrapper import MotionPlanningWrapper
+
+
+def print_mp_info():
+    """
+    Prints out useful information for user for knowing how to use this MP demo
+    """
+
+    def print_command(char, info):
+        char += " " * (10 - len(char))
+        print("{}\t{}".format(char, info))
+
+    print()
+    print("*" * 30)
+    print("MOTION PLANNING DEMO")
+    print("*" * 30)
+    print()
+    print("Use your mouse to generate and execute motion plans:")
+    print()
+    print_command(
+        "Left click",
+        "Create (click), visualize (drag) and plan / execute (release) a base motion subgoal "
+        "for the robot base to reach the physical point that corresponds to the clicked pixel",
+    )
+    print_command(
+        "Middle click",
+        "Create, and plan / execute an arm motion subgoal for the robot end-effector "
+        "to reach the physical point that corresponds to the clicked pixel",
+    )
+    print()
+    print("Press 'm' to toggle between viewer modes:")
+    print()
+    print_command("Nav mode", "Enables direct robot navigation and movement of viewer camera")
+    print_command("Manip mode", "Enables direct manipulation of objects within the scene")
+    print_command("Planning mode", "Enables generation and execution of motion plans (default)")
+    print()
+    print("Press 'h' for on-screen help.")
+    print()
+    print("*" * 30)
 
 
 def run_example(args):
@@ -16,6 +55,15 @@ def run_example(args):
 
     motion_planner = MotionPlanningWrapper(nav_env)
     state = nav_env.reset()
+
+    # Set viewer camera and directly set mode to planning mode
+    nav_env.simulator.viewer.initial_pos = [1.5, 0.1, 2.3]
+    nav_env.simulator.viewer.initial_view_direction = [-0.7, -0.3, -0.6]
+    nav_env.simulator.viewer.reset_viewer()
+    nav_env.simulator.viewer.mode = ViewerMode.PLANNING
+
+    # Print out helpful information for this demo
+    print_mp_info()
 
     while True:
         action = np.zeros(nav_env.action_space.shape)
