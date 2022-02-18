32cbb5ca0091009c6bfcbccbf67a76b53c46d5ef
Michael Lingelbach <m.j.lbach@gmail.com>
2022-02-17 17:09:14 -0800

    ci: fix dockerfile (#751)
    * ci: fix dockerfile
    
    * ci: fix dockerfile
    
    * update doc
    
    * Updates
    
    * Update
    
    * Make runnable
    
    * Rename
    
    * Cleanups
    
    * Update
    
    * updates
    
    Co-authored-by: Chengshu Li <chengshu@stanford.edu>

--- docker/tensorflow/build.sh
+++ docker/tensorflow/build.sh
@@ -2,7 +2,4 @@
 
 IMAGE=igibson/igibson
 
-export DOCKER_BUILDKIT=1
-docker build -t $IMAGE:v1.0.1 . \
-    && docker tag $IMAGE:v1.0.1 $IMAGE:latest \
-    && echo BUILD SUCCESSFUL
+docker build -t $IMAGE .
