58ac14cf62949008b6851a5a95602cd5084edffd
Cem Gökmen <1408354+cgokmen@users.noreply.github.com>
2022-05-09 00:36:04 -0700

    Bump version to 2.2.0 (#782)

--- setup.py
+++ setup.py
@@ -129,7 +129,7 @@ else:
 
 setup(
     name="igibson",
-    version="2.1.0",
+    version="2.2.0",
     author="Stanford University",
     long_description_content_type="text/markdown",
     long_description=long_description,
