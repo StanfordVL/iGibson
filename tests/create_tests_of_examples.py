354658a988d080adafdfd8898b0f97e5847f6959
Roberto <rmartinmar@gmail.com>
2022-06-24 15:42:14 -0700

    cleanup

--- tests/create_tests_of_examples.py
+++ tests/create_tests_of_examples.py
@@ -23,7 +23,6 @@ def main(exhaustive=False):
             and "web_ui" not in package.name[17:]  # The WebUI examples require additional server setup
             and "vr_" not in package.name[17:]  # The VR examples require additional dependencies
             and "ray_" not in package.name[17:]  # The Ray/RLLib example does not run in a subprocess
-            and "skill" not in package.name[17:]  # The action primitives training code needs to be cleanup first
         ):  # Consider removing the last condition if we have runnable VR tests
             examples_list += [package.name[17:]]
 
