bfa09f7735c96b04e79288028ec2bb528e01a23c
monaanvari <mona.anvari2011@gmail.com>
2022-08-09 23:47:16 -0700

    rgb for now

--- igibson/examples/scenes/example_script_b1kdataset.py
+++ igibson/examples/scenes/example_script_b1kdataset.py
@@ -2,6 +2,9 @@ import logging
 from sys import platform
 
 import numpy as np
+import pybullet as p
+# from PIL import Image
+import cv2
 
 from igibson.render.mesh_renderer.mesh_renderer_settings import MeshRendererSettings
 from igibson.render.profiler import Profiler
@@ -10,58 +13,71 @@ from igibson.simulator import Simulator
 from igibson.utils.assets_utils import get_available_ig_scenes
 from igibson.utils.utils import let_user_pick
 
+RENDER_WIDTH = 1080
+RENDER_HEIGHT = 720
 
-def main(selection="user", headless=False, short_exec=False):
+def main( headless=True, short_exec=False):
     """
     Prompts the user to select any available interactive scene and loads it.
     Shows how to load directly scenes without the Environment interface
     Shows how to sample points in the scene by room type and how to compute geodesic distance and the shortest path
     """
-    print("*" * 80 + "\nDescription:" + main.__doc__ + "*" * 80)
     available_ig_scenes = get_first_options()
-    scene_id = available_ig_scenes[let_user_pick(available_ig_scenes, selection=selection) - 1]
+    scene_id = available_ig_scenes[12]
     settings = MeshRendererSettings(enable_shadow=True, msaa=False)
     if platform == "darwin":
         settings.texture_scale = 0.5
     s = Simulator(
         mode="gui_interactive" if not headless else "headless",
-        image_width=512,
-        image_height=512,
-        rendering_settings=settings,
+        image_width=1080, image_height=720, vertical_fov=60,
+        rendering_settings=settings
     )
 
     scene = InteractiveIndoorScene(
         scene_id,
-        # load_object_categories=[],  # To load only the building. Fast
         build_graph=True,
     )
     s.import_scene(scene)
 
-    # Shows how to sample points in the scene
-    np.random.seed(0)
-    for _ in range(10):
-        pt = scene.get_random_point_by_room_type("living_room")[1]
-        print("Random point in living_room: {}".format(pt))
+    random_floor = scene.get_random_floor()
+    for i in range(10):
 
-    for _ in range(10):
         random_floor = scene.get_random_floor()
-        p1 = scene.get_random_point(random_floor)[1]
-        p2 = scene.get_random_point(random_floor)[1]
-        shortest_path, geodesic_distance = scene.get_shortest_path(random_floor, p1[:2], p2[:2], entire_path=True)
-        print("Random point 1: {}".format(p1))
-        print("Random point 2: {}".format(p2))
-        print("Geodesic distance between p1 and p2: {}".format(geodesic_distance))
-        print("Shortest path from p1 to p2: {}".format(shortest_path))
+        camera_pos = scene.get_random_point(random_floor)[1]
+        camera_pitch , camera_yaw, camera_roll = 0, 0, np.random.uniform(low=-np.pi/4, high=np.pi/18)
+        camera_target_pos = camera_pos + [0, 0, (camera_pos[1] + (1 /np.cos(camera_roll)))]
+        # s.renderer.set_camera(camera_pos, camera_orn)
+        view_matrix = p.computeViewMatrixFromYawPitchRoll(
+        cameraTargetPosition=camera_pos,
+        distance=1,
+        yaw=camera_yaw,
+        pitch=camera_roll,
+        roll=camera_pitch,
+        upAxisIndex=2)
+        proj_matrix = p.computeProjectionMatrixFOV(
+        fov=60, aspect=float(RENDER_WIDTH) / RENDER_HEIGHT,
+        nearVal=0.1, farVal=100.0)
+
+        _, _, rgba, depth, segmask  = p.getCameraImage(width=RENDER_WIDTH,
+                                                       height=RENDER_HEIGHT,
+                                                       viewMatrix=view_matrix,
+                                                       projectionMatrix=proj_matrix,
+                                                       shadow=1,
+                                                       flags=p.ER_SEGMENTATION_MASK_OBJECT_AND_LINKINDEX,
+                                                       renderer=p.ER_BULLET_HARDWARE_OPENGL)
+
+
+        rgba = np.array(rgba).astype('uint8')
+        rgba = rgba.reshape((RENDER_HEIGHT, RENDER_WIDTH, 4))
+        rgb = rgba[:, :, :3]
+        rgb = rgb[:,:,::-1]
+
+        rgb = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
+        cv2.imwrite('/scr/monaavr/test_img/rgbtest' + str(i) + '.jpg', rgb )
+
 
     if not headless:
         input("Press enter")
-
-    max_steps = -1 if not short_exec else 1000
-    step = 0
-    while step != max_steps:
-        with Profiler("Simulator step"):
-            s.step()
-            step += 1
 
     s.disconnect()
 
