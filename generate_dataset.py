03419523f6515372e012bdbe2dfcd69fee0b2a91
macvincent <macvincentaghaoko@gmail.com>
2022-09-28 19:03:43 -0700

    Fix bug in image generation

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,6 +1,5 @@
 import os
 
-import cv2
 import h5py
 import numpy as np
 
--- generate_video_from_dataset.py
+++ generate_video_from_dataset.py
@@ -1,13 +1,15 @@
-import h5py
 import cv2
+import h5py
 import numpy as np
+from PIL import Image
 
 filename = "data.hdf5"
-frame_size = (1024,720)
-fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-output_video = cv2.VideoWriter('output_video.mp4',fourcc, 20.0, frame_size)
+frame_size = (1024, 720)
+fourcc = cv2.VideoWriter_fourcc(*"MP4V")
+output_video = cv2.VideoWriter("output_video.mp4", fourcc, 20.0, frame_size)
 
 with h5py.File(filename, "r") as dataset:
-    for img in dataset['rgb']:
-        output_video.write(np.uint8(img[:, :, :3]))
+    for img in dataset["rgb"]:
+        img = np.array(Image.fromarray((255 * img[:, :, :3]).astype(np.uint8)))
+        output_video.write(cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
     output_video.release()
