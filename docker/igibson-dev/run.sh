807258d173cd7a1e0808b8c002bfbe63e276796f
Cem Gökmen <cem@cemgokmen.com>
2022-10-14 20:32:46 -0700

    Update the run script to include entrypoint name.

--- docker/igibson-dev/run.sh
+++ docker/igibson-dev/run.sh
@@ -24,7 +24,12 @@ echo "working directory = "$SLURM_SUBMIT_DIR
 # Then, create a container.
 enroot create -n igibson /cvgl/group/igibson-docker/igibson-dev.sqsh && {
   # Run the container, mounting iGibson at the right spot
-  enroot start -r -w -m ${IG_IGIBSON_PATH}:/igibson -m ${IG_OUTPUT_PATH}:/out -e SLURM_LOCALID=${SLURM_LOCALID} igibson python -m ${IG_ENTRYPOINT_MODULE};
+  enroot start -r -w \
+    -m ${IG_IGIBSON_PATH}:/igibson \
+    -m ${IG_OUTPUT_PATH}:/out \
+    -e SLURM_LOCALID=${SLURM_LOCALID} \
+    -e IG_ENTRYPOINT_MODULE=${IG_ENTRYPOINT_MODULE} \
+    igibson;
   # Remove the container.
   enroot remove -f igibson;
 }
